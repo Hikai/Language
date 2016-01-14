368f7b45928cb11f743c1834d1ae19f26e2e1652
HiKi <bahyun.lee@gmail.com>
2016-01-14 11:40:03 +0900

    strpos
    complete.

--- Php/proc_num_include.php
+++ Php/proc_num_include.php
@@ -1,5 +1,13 @@
 <?php
-$find_num = isset($_POST["number"]) ? $_POST["number"] : false;
-$max = isset($_POST["max_num"]) ? $_POST["max_num"] : false;
-echo($find_num."<br />".$max);
+$find_num_int = isset($_POST["number"]) ? $_POST["number"] : false;
+$max_int = isset($_POST["max_num"]) ? $_POST["max_num"] : false;
+$find = (string) $find_num_int;
+$max = (string) $max_int;
+for ($i = 0 ; $i < $max_int; $i++) {
+	if (strpos((string)$i, $find) !== false) {
+		echo($i." ");
+	} else {
+		continue;
+	}
+}
 ?>
