260a881c1a397348754b4bec9327629aed3a778e
HiKi <bahyun.lee@gmail.com>
2016-01-24 01:47:32 +0900

    160123 prob
    go prob

--- Go/160123_prob1.go
+++ Go/160123_prob1.go
@@ -12,7 +12,7 @@ func main() {
 	key_back := [7]int{108, 108, 104, 105, 107, 97, 105}
 	url := [6]int{113, 72, 86, 106, 112, 70}
 	var result int;
-	fmt.Println("Key : hiki?");
+	fmt.Println("Key : hiki + ?");
 	if (len(os.Args) < 2) || (len(os.Args) != 8) {
 		os.Exit(1);
 	}
@@ -31,5 +31,6 @@ func main() {
 		for i := 0; i < 6; i++ {
 			fmt.Print(string(url[i]))
 		}
+		fmt.Println("\nLanguage");
 	}
 }
