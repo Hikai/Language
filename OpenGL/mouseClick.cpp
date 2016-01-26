07e8ff22a26d7862a8da9f6075555517f3b8f1ed
HiKi <bahyun.lee@gmail.com>
2016-01-27 02:32:43 +0900

    color, line, optimization
    / color : red -> yellow
    + line_strip
    / glutPostRediplay position modify

--- OpenGL/mouseClick.cpp
+++ OpenGL/mouseClick.cpp
@@ -18,7 +18,11 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
 void do_display()
 {
 	glClear(GL_COLOR_BUFFER_BIT);
-	glColor3f(1, 0, 0);
+	glColor3f(1, 1, 0);
+	glBegin(GL_LINE_STRIP);
+	glVertex2f(-1.0, 0.7);
+	glVertex2f(-0.6, 0.7);
+	glEnd();
 	glBegin(GL_POLYGON);
 	glVertex3f(0.5, 0.5, 0);
 	glVertex3f(-0.5, 0.5, 0);
@@ -32,13 +36,12 @@ void do_mouse(int button, int state, int x, int y)
 {
 	char coordination[128];
 	if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN && x < 170 && y < 90) {
-		sprintf_s(coordination, "x = %d, y = %d, good", x, y);
+		sprintf_s(coordination, "x = %d, y = %d", x, y);
 		glClearColor(1, 1, 1, 1);
 		glutSetWindowTitle(coordination);
-		glutPostRedisplay();
 	}
 	else {
 		glClearColor(0, 0, 0, 0);
-		glutPostRedisplay();
 	}
+	glutPostRedisplay();
 }
