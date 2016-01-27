d3298c9ca0f220802f24ce0e1ac13a6689ca290a
HiKi <bahyun.lee@gmail.com>
2016-01-28 00:01:17 +0900

    text print
    + text print

--- OpenGL/mouseClick.cpp
+++ OpenGL/mouseClick.cpp
@@ -4,6 +4,9 @@
 
 void do_display();
 void do_mouse(int, int, int, int);
+void printw(float, float, float, char *, ...);
+
+GLvoid *font_style = GLUT_BITMAP_TIMES_ROMAN_24;
 
 int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
@@ -29,6 +32,7 @@ void do_display()
 	glVertex3f(0.5, -0.5, 0);
 	glVertex3f(-0.5, -0.5, 0);
 	glEnd();
+	printw(-0.9, 0.8, 0, "Click range");
 	glFlush();
 }
 
@@ -41,7 +45,26 @@ void do_mouse(int button, int state, int x, int y)
 		glutSetWindowTitle(coordination);
 	}
 	else {
-		glClearColor(0, 0, 0, 0);
+		glClearColor(0, 0, 0, 1);
 	}
 	glutPostRedisplay();
+}
+
+// https://github.com/alibad/Blog/blob/master/OpenGL/OpenGL%20Printw/OpenGL%20Printw/printw.c
+void printw(float x, float y, float z, char * format, ...)
+{
+	va_list args;
+	int len;
+	int i;
+	char * text;
+	va_start(args, format);
+	len = _vscprintf(format, args) + 1;
+	text = (char *)malloc(len * sizeof(char));
+	vsprintf_s(text, len, format, args);
+	va_end(args);
+	glRasterPos3f(x, y, z);
+	for (i = 0; text[i] != '\0'; i++) {
+		glutBitmapCharacter(font_style, text[i]);
+	}
+	free(text);
 }
