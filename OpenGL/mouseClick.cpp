4c1e17ab5e79c088914f5c72955500b5d3cd67f9
HiKi <bahyun.lee@gmail.com>
2016-01-29 01:14:55 +0900

    add range
    + add second range.

--- OpenGL/mouseClick.cpp
+++ OpenGL/mouseClick.cpp
@@ -21,10 +21,22 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
 void do_display()
 {
 	glClear(GL_COLOR_BUFFER_BIT);
-	glColor3f(1, 1, 0);
+	glColor3f(1, 0, 0);
 	glBegin(GL_LINE_STRIP);
 	glVertex2f(-1.0, 0.7);
-	glVertex2f(-0.6, 0.7);
+	glVertex2f(-0.57, 0.7);
+	glEnd();
+	glBegin(GL_LINE_STRIP);
+	glVertex2f(-0.57, 0.7);
+	glVertex2f(-0.15, 0.7);
+	glEnd();
+	glBegin(GL_LINE_STRIP);
+	glVertex2f(-0.57, 0.7);
+	glVertex2f(-0.57, 1.0);
+	glEnd();
+	glBegin(GL_LINE_STRIP);
+	glVertex2f(-0.15, 0.7);
+	glVertex2f(-0.15, 1.0);
 	glEnd();
 	glBegin(GL_POLYGON);
 	glVertex3f(0.5, 0.5, 0);
@@ -32,17 +44,21 @@ void do_display()
 	glVertex3f(0.5, -0.5, 0);
 	glVertex3f(-0.5, -0.5, 0);
 	glEnd();
-	printw(-0.9, 0.8, 0, "Click range");
+	printw(-0.93, 0.8, 0, "Click range");
+	printw(-0.52, 0.8, 0, "Second range");
 	glFlush();
 }
 
 void do_mouse(int button, int state, int x, int y)
 {
 	char coordination[128];
-	if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN && x < 170 && y < 90) {
-		sprintf_s(coordination, "x = %d, y = %d", x, y);
-		glClearColor(1, 1, 1, 1);
-		glutSetWindowTitle(coordination);
+	if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN) {
+		if ((x <= 170) && (y <= 90)) {
+			glClearColor(1, 0, 1, 1);
+		}
+		if ((170 < x) && (x <= 340) && (y <= 90)) {
+			glClearColor(1, 1, 0, 1);
+		}
 	}
 	else {
 		glClearColor(0, 0, 0, 1);
