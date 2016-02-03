523181c2a74eb0c1c3ed06b5fbd02ecbd0f4efa6
Bahyun Lee <bahyun.lee@gmail.com>
2016-02-04 01:07:25 +0900

    include library
    / move code to library.

--- Php/ip.php
+++ Php/ip.php
@@ -1,19 +1,5 @@
 <?php
-	$ip = $_SERVER["REMOTE_ADDR"];
-	$last_dot_index = strrpos($ip, ".");
-	$second_last_dot = strrpos(substr($ip, 0, $last_dot_index - 1), ".");
-	echo($ip."<br />");
-	for ($i = 1; $i < 3; $i++) {
-		if($second_last_dot + $i == $last_dot_index) {
-			break;
-		}
-		$ip[$second_last_dot + $i] = '*';
-	}
-	for ($i = 1; $i < 3; $i++) {
-		if ($ip[$last_dot_index + $i] == '') {
-			break;
-		}
-		$ip[$last_dot_index + $i] = '*';
-	}
-	echo($ip);
+include("./lib.php");
+$ip = $_SERVER["REMOTE_ADDR"];
+print_ip($ip);
 ?>
