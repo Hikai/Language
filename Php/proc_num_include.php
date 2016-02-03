289925aed1760eb632ee50ebd77e8452452bd664
Bahyun Lee <bahyun.lee@gmail.com>
2016-02-04 01:09:25 +0900

    include library
    / move code to library.

--- Php/proc_num_include.php
+++ Php/proc_num_include.php
@@ -1,13 +1,6 @@
 <?php
+include("./lib.php");
 $find_num_int = isset($_POST["number"]) ? $_POST["number"] : false;
 $max_int = isset($_POST["max_num"]) ? $_POST["max_num"] : false;
-$find = (string) $find_num_int;
-$max = (string) $max_int;
-for ($i = 0 ; $i < $max_int; $i++) {
-	if (strpos((string)$i, $find) !== false) {
-		echo($i." ");
-	} else {
-		continue;
-	}
-}
+include_num($find_num_int, $max_int);
 ?>
