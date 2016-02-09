4dc758d22027406557d3899bf68e17a1d599dcd9
Bahyun Lee <bahyun.lee@gmail.com>
2016-02-09 13:04:22 +0900

    php class edit
    - method "primt_info"
    + method "get_char_mp(hp)"

--- Php/class.php
+++ Php/class.php
@@ -1,19 +1,30 @@
 <?php
 class Character {
-	private $char_hp, $char_mp;
-	function set_char_hp($hp) {
-		$char_hp = $hp;
+	private $char_hp = 0, $char_mp = 0;
+	function get_char_mp() {
+		return $char_mp;
 	}
-	function set_char_mp($mp) {
-		$char_mp = $mp;
+	function get_char_hp() {
+		return $char_hp;
 	}
-	function print_info() {
-		echo($char_hp."<br />".$char_mp);
+	function set_char_mp($mp, $mp_minus) {
+		if ($char_mp === 0) {
+			$char_mp = $mp;
+		}
+		$char_mp -= $mp_minus;
+	}
+	function set_char_hp($hp, $hp_minus) {
+		if ($char_hp === 0) {
+			$char_hp = $hp;
+		}
+		$char_hp -= $hp_minus;
 	}
 }
 
 $char = new Character;
-$char->set_char_hp(123);
-$char->set_char_mp(321);
-$char->print_info();
+$char->set_char_hp(123, 0);
+$char->set_char_mp(321, 0);
+$mp = $char->get_char_mp();
+$hp = $char->get_char_hp();
+echo($hp."<br />".$mp);
 ?>
