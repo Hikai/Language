4e4f94ecf76cc2f5e78092517feb841d3b303043
Bahyun Lee <bahyun.lee@gmail.com>
2016-02-11 20:44:05 +0900

    include library
    + include library.

--- Php/list.php
+++ Php/list.php
@@ -1,7 +1,6 @@
 <?php
+include("./lib.php");
 header("Content-Type: text/html; charset=UTF-8");
-$list = scandir("DIR");
-for ($i = 0; $i < count($list); $i++) {
-	echo($list[$i]."<br />");
-}
+$dir = '/var/www/html';
+dir_file_lists($dir);
 ?>
