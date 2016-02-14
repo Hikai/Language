ddc31e2c98b4a12ba9819fdeccdf68cd122c8a05
Bahyun Lee <bahyun.lee@gmail.com>
2016-02-14 16:26:48 +0900

    php session login page
    + post login form.

---
+++ Php/session_login.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+  <body>
+    <form action="proc_session_login.php" method="post">
+      id : <input name="id" type="text" />
+      pw : <input name="pw" type="password" />
+      <input type="submit" />
+    </form>
+  </body>
+</html>
