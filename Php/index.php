a29466ed810096a5dc354c5c214598826c339c2b
HiKi <bahyun.lee@gmail.com>
2016-02-21 02:33:59 +0900

    include lib, login process
    + include library.
    + login process.

--- Php/index.php
+++ Php/index.php
@@ -8,7 +8,8 @@ session_start();
 		<title></title>
 	</head>
 <?php
-(!isset($_SESSION["char_name"]) || !isset($_SESSION["char_pw"])) {
+if (!isset($_SESSION["char_name"]) || !isset($_SESSION["char_pw"])) {
+	include("./lib.php");
 ?>
 	<body>
 		<form action="index.php" method="post">
@@ -19,6 +20,19 @@ session_start();
 	</body>
 </html>
 <?php
+	if (!isset($_POST["char_name"]) || !isset($_POST["char_pw"])) {
+		exit;
+	}
+	$name = $_POST["char_name"];
+	$pw = $_POST["char_pw"];
+	conn_db("password", "game");
+	$query = "select * from game_account where id=\"".$name."\" and pw=password('".$pw."');";
+	$array_query_result = sql_query($query);
+	if ($array_query_result["name"] == $name) {
+		$_SESSION["char_name"] = $name;
+		$_SESSION["char_pw"] = $pw;
+		header("Refresh: 0");
+	}
 } else {
 ?>
 	<body>
