9570c00efd7f3e9cebab84d33e91dee54e3b2001
HiKi <bahyun.lee@gmail.com>
2016-02-22 01:44:33 +0900

    include lib, query
    + include lib.php.
    + insert query.

--- Php/create_character.php
+++ Php/create_character.php
@@ -1,4 +1,5 @@
 <?php
+include("./lib.php");
 class Character {
 	private $level = 0, $hp = 0, $mp = 0;
 	private $name = "";
@@ -36,16 +37,24 @@ class Character {
 	<body>
 		<form action="create_character.php" method="post">
 			name : <input name="name" type="text" /><br />
+			pw : <input name="pw" type="password" /><br />
 			<input type="submit" />
 		</form>
 	</body>
 </html>
 <?php
 $name = (isset($_POST["name"])) ? $_POST["name"] : "";
-if ($name === "") {
+$pw = (isset($_POST["pw"])) ? $_POST["pw"] : "";
+if ($name === "" || $pw === "") {
 	exit;
 }
 $character = new Character;
 $character->set_name($name);
-echo($character->get_name());
+conn_db("password", "game");
+$query = "insert into game_account (name, pw) values (\"".$name."\", password(\"".$pw."\"))";
+$array_query_result = sql_query($query);
+if ($array_query_result) {
+	header("location: ./index.php");
+	exit;
+}
 ?>
