eb9d2a07b90c844b1065a7708dfde77b7be8bd76
HiKi <bahyun.lee@gmail.com>
2016-02-22 01:44:07 +0900

    name, session
    + session check.
    / edit parameter name.

--- Php/index.php
+++ Php/index.php
@@ -1,11 +1,13 @@
 <?php
-session_start();
+if (!isset($_SESSION)) {
+	session_start();
+}
 ?>
 <!DOCTYPE html>
 <html lang="ko">
 	<head>
 		<meta charset="utf-8" />
-		<title></title>
+		<title>game</title>
 	</head>
 <?php
 if (!isset($_SESSION["char_name"]) || !isset($_SESSION["char_pw"])) {
@@ -13,24 +15,25 @@ if (!isset($_SESSION["char_name"]) || !isset($_SESSION["char_pw"])) {
 ?>
 	<body>
 		<form action="index.php" method="post">
-			character name : <input name="char_name" type="text" />
-			password : <input name="char_pw" type="password" /> <br />
+			character name : <input name="name" type="text" />
+			password : <input name="pw" type="password" /> <br />
 			<input type="submit" />
 		</form>
+		<a href="create_character.php">create character</a>
 	</body>
 </html>
 <?php
-	if (!isset($_POST["char_name"]) || !isset($_POST["char_pw"])) {
+	if (!isset($_POST["name"]) || !isset($_POST["pw"])) {
 		exit;
 	}
-	$name = $_POST["char_name"];
-	$pw = $_POST["char_pw"];
+	$name = $_POST["name"];
+	$pw = $_POST["pw"];
 	conn_db("password", "game");
-	$query = "select * from game_account where id=\"".$name."\" and pw=password('".$pw."');";
+	$query = "select * from game_account where name=\"".$name."\" and pw=password('".$pw."');";
 	$array_query_result = sql_query($query);
 	if ($array_query_result["name"] == $name) {
-		$_SESSION["char_name"] = $name;
-		$_SESSION["char_pw"] = $pw;
+		$_SESSION["name"] = $name;
+		$_SESSION["pw"] = $pw;
 		header("Refresh: 0");
 	}
 } else {
