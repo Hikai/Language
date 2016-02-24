14784a79d857688d9d1a71953791364f81a0e8c3
HiKi <bahyun.lee@gmail.com>
2016-02-25 00:26:44 +0900

    session check
    + session check.

--- Php/info.php
+++ Php/info.php
@@ -2,10 +2,13 @@
 if (!isset($_SESSION)) {
 	session_start();
 }
+if (!isset($_SESSION["name"]) || !isset($_SESSION["pw"]) || !isset($_SESSION["level"])) {
+	exit;
+}
 echo("Level : ".$_SESSION["level"]."<br />");
 $skills = array("찌르기", "", "연속베기", "");
 echo("Skills :<br />");
-for ($i = 0; $i < $_SESSION["level"]; $i++);
+for ($i = 0; $i < $_SESSION["level"]; $i++) {
 	if ($item === "") {
 		continue;
 	}
