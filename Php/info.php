d7ebe1a0a0b169b00ffa84b398d1aa9f12e0c153
HiKi <bahyun.lee@gmail.com>
2016-02-26 02:36:05 +0900

    redirect
    + session check redirect.

--- Php/info.php
+++ Php/info.php
@@ -3,6 +3,7 @@ if (!isset($_SESSION)) {
 	session_start();
 }
 if (!isset($_SESSION["name"]) || !isset($_SESSION["pw"]) || !isset($_SESSION["level"])) {
+	header("Location: index.php");
 	exit;
 }
 echo("Level : ".$_SESSION["level"]."<br />");
