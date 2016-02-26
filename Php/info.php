5000bd57eba7b6b7909edc2dccdf1d5605804732
HiKi <bahyun.lee@gmail.com>
2016-02-26 15:25:00 +0900

    a few edit
    / edit array
    / edit for syntax.

--- Php/info.php
+++ Php/info.php
@@ -7,9 +7,9 @@ if (!isset($_SESSION["name"]) || !isset($_SESSION["pw"]) || !isset($_SESSION["le
 	exit;
 }
 echo("Level : ".$_SESSION["level"]."<br />");
-$skills = array("찌르기", "", "연속베기", "");
-echo("Skills :<br />");
-for ($i = 0; $i < $_SESSION["level"]; $i++) {
+$skills = array("1", "", "2", "", "3");
+echo("Able skills :<br />");
+for ($i = 0; $i <= $_SESSION["level"]; $i++) {
 	if ($item === "") {
 		continue;
 	}
