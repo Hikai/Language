46de1872b3c5df165dbf0c3b5f928afdf0bf5a74
HiKi <bahyun.lee@gmail.com>
2016-03-07 21:59:35 +0900

    add submit, echo info
    + add submit in form.
    + echo information data.

--- Php/party_match.php
+++ Php/party_match.php
@@ -7,9 +7,19 @@ include("lib.php");
 	</head>
 	<body>
 		<form action="party_match.php" method="post">
-			제목 : <input name="subject" type="text" />
+			제목 : <input name="subject" maxlength="20" type="text" />
 			목적 : <input name="purpose" type="text" />
 			한계 인원 : <input name="limit_person" maxlength="2" type="text" />
+			<input type="submit" />
 		</form>
 	</body>
 </html>
+<?php
+if (!isset($_POST["subject"]) || !isset($_POST["purpose"]) || !isset($_POST["limit_person"]) || !is_numeric($_POST["limit_person"])) {
+	exit;
+}
+$subject = $_POST["subject"];
+$purpose = $_POST["purpose"];
+$limit_person = $_POST["limit_person"];
+echo("Subject : ".$subject."<br />Purpose : ".$purpose."<br />Limit person : ".$limit_person);
+?>
