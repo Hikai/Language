7aff0190f230cbd198cf33d083aa93ae3dc9d509
HiKi <bahyun.lee@gmail.com>
2016-03-14 00:43:20 +0900

    session check, print list
    + session check.
    + print party list.

--- Php/party_match.php
+++ Php/party_match.php
@@ -1,5 +1,13 @@
 <?php
+if (!isset($_SESSION)) {
+	session_start();
+}
+if (!isset($_POST["subject"]) || !isset($_POST["purpose"]) || !isset($_POST["limit_person"]) || !is_numeric($_POST["limit_person"])) {
+	header("Location: index.php");
+	exit;
+}
 include("lib.php");
+conn_db("password", "game");
 ?>
 <html>
 	<head>
@@ -12,17 +20,21 @@ include("lib.php");
 			한계 인원 : <input name="limit_person" maxlength="2" type="text" />
 			<input type="submit" />
 		</form>
+<?php
+$query = "select * from game_party_match";
+$query_result = @mysql_query($query) or die("query error");
+while ($array_result = mysql_fetch_array($query_result)) {
+	echo("ID : ".$array_result["id"]."<br />Subject : ".$array_result["subject"]."<br /> Purpose : ".$array_result["purpose"]."<br />".$array_result["now_person"]." / ".$array_result["limit_person"]."<br />");
+
+}
+?>
 	</body>
 </html>
 <?php
-if (!isset($_POST["subject"]) || !isset($_POST["purpose"]) || !isset($_POST["limit_person"]) || !is_numeric($_POST["limit_person"])) {
-	exit;
-}
 $subject = $_POST["subject"];
 $purpose = $_POST["purpose"];
 $limit_person = $_POST["limit_person"];
 echo("Subject : ".$subject."<br />Purpose : ".$purpose."<br />Limit person : ".$limit_person);
-conn_db("password", "game");
 $query = "insert into game_party_match (id, subject, purpose, limit_person) values (\""."admin"."\", \"".$subject."\", \"".$purpose."\", \"".$limit_person."\")";
 $query_result = sql_query($query);
 ?>
