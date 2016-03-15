1b2948372750c7d534b3ca5aed0316389f4127b3
HiKi <bahyun.lee@gmail.com>
2016-03-16 00:04:09 +0900

    combine session_login and proc_sess~
    + add proc_session_login.php source code.

--- Php/session_login.php
+++ Php/session_login.php
@@ -1,3 +1,12 @@
+<?php
+if (!isset($_SESSION)) {
+	session_start();
+}
+$se_id = "admin";
+$se_pw = "administrator";
+$id = "";
+$pw = "";
+?>
 <!DOCTYPE html>
 <html>
   <body>
@@ -8,3 +17,15 @@
     </form>
   </body>
 </html>
+<?php
+if ((!isset($_POST["id"])) || (!isset($_POST["pw"]))) {
+	exit;
+} else {
+	$id = $_POST["id"];
+	$pw = $_POST["pw"];
+}
+if (($id == $se_id) && (strcmp($pw, $se_pw) === 0)) {
+	$_SESSION["id"] = $id;
+	$_SESSION["pw"] = $pw;
+}
+?>
