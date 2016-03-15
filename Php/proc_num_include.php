691a5df04aeac8e99689c75e344eb22f90eecc6d
Bahyun Lee <bahyun.lee@gmail.com>
2016-03-15 13:49:11 +0900

    delete proc_num_include.php
    - delete proc_num_include.php file.

--- Php/proc_num_include.php
+++
@@ -1,6 +0,0 @@
-<?php
-include("./lib.php");
-$find_num_int = isset($_POST["number"]) ? $_POST["number"] : false;
-$max_int = isset($_POST["max_num"]) ? $_POST["max_num"] : false;
-include_num($find_num_int, $max_int);
-?>
