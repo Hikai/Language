3189c306640da01feb0653bbba7bf8899bf77256
Bahyun Lee <bahyun.lee@gmail.com>
2016-03-15 13:48:47 +0900

    combine num_include.php and proc_num_include.php
    + add proc_num_include.php file source.
    + variable false check.

--- Php/num_include.php
+++ Php/num_include.php
@@ -17,3 +17,11 @@
 		</form>
 	</body>
 </html>
+<?php
+$find_num_int = isset($_POST["number"]) ? $_POST["number"] : false;
+$max_int = isset($_POST["max_num"]) ? $_POST["max_num"] : false;
+if (($find_num_int == false) || ($max_int == false)) {
+	exit;
+}
+include_num($find_num_int, $max_int);
+?>
