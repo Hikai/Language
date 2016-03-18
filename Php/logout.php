b295084e2eb4c54f51ac06594e92dfbde45c8563
HiKi <bahyun.lee@gmail.com>
2016-03-18 19:58:06 +0900

    session check
    + add session check.

--- Php/logout.php
+++ Php/logout.php
@@ -2,6 +2,10 @@
 if (!isset($_SESSION)) {
 	session_start();
 }
+if (!isset($_SESSION["name"]) || !isset($_SESSION["pw"]) || !isset($_SESSION["level"])) {
+	header("Location: index.php");
+	exit;
+}
 session_destroy();
 header("Location: index.php");
 ?>
