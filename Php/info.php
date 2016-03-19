44f7d0a13a1dc80d4c5861781c62a6c7d4db036c
HiKi <bahyun.lee@gmail.com>
2016-03-20 00:08:43 +0900

    remove comment, add query
    - remove comment.
    + add query (attack, defence, critical, avoid data).

--- Php/info.php
+++ Php/info.php
@@ -7,8 +7,12 @@ if (!isset($_SESSION["name"]) || !isset($_SESSION["pw"]) || !isset($_SESSION["le
 	exit;
 }
 echo("Level : ".$_SESSION["level"]."<br />");
-// Attack
-// Defence
-// Critical
-// Avoid
+include("lib.php");
+conn_db("password", "game");
+$query = "select attack, defence, critical, avoid from game_status where name=\"".$_SESSION["name"]."\" and level=".$_SESSION["level"]."limit 1";
+$query_result = sql_query($query);
+if (empty($query_result)) {
+	exit;
+}
+echo("Attack : ".$query_result["attack"]."<br />Defence : ".$query_result["defence"]."<br />Critical : ".$query_result["critical"]."<br />Avoid : ".$query_result["avoid"]);
 ?>
