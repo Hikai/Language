37d691ec994ac195d9d051a94aa70f89d59bc414
HiKi <bahyun.lee@gmail.com>
2016-03-22 00:10:07 +0900

    remove class, include library
    - remove Mob class.
    + include lib.php.

--- Php/in.php
+++ Php/in.php
@@ -6,26 +6,11 @@ if (!isset($_SESSION["name"]) || !isset($_SESSION["pw"]) || !isset($_SESSION["le
 	header("Location: index.php");
 	exit;
 }
-class Mob {
-	private $mob_hp = 0, $mob_defen = 0, $mob_attk = 0;
-	function __construct() {
-		$user_level = (int)$_SESSION["level"];
-		$this->mob_hp = mt_rand($level, $level * 10);
-		$this->mob_defen = $level;
-		$this->mob_attk = $level * 1.5;
-	}
-	function get_attk() {
-		return $this->mob_attk;
-	}
-	function get_defen() {
-		return $this->mob_defen;
-	}
-	function get_hp() {
-		return $this->mob_hp;
-	}
-}
-
-$mob = new Mob;
+include("lib.php");
+$query = "select attack, defence, critical, avoid from game_status where name=\"".$_SESSION["name"]."\" and level=".$_SESSION["level"];
+$level = (int)$_SESSION["level"];
+$mob = new Mob($level);
 echo("HP : ".$mob->get_hp()."<br />Defence : ".$mob->get_defen()."<br />Attack : ".$mob->get_attk());
+// Battle code.
 unset($mob);
 ?>
