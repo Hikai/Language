d2f0ddb7a2cf513df2483109028f62b01a35e60a
Bahyun Lee <bahyun.lee@gmail.com>
2016-03-27 00:42:31 +0900

    kind of variable in rust
    + various variable allocate way.

---
+++ Rust/variable.rs
@@ -0,0 +1,8 @@
+fn main() {
+	let (a, b) = (1, 2);
+	let c: i32 = 3;
+	let d = 4; // d: i32
+	let e = 5; // e is not change.
+	let mut f = 6; // f able change.
+	println!("{}, {}, {}, {}, {}, {}", a, b, c, d, e, f);
+}
