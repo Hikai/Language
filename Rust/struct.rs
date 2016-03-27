5150e4b92eaf207109c5d5f82c9e810f86c8de50
Bahyun Lee <bahyun.lee@gmail.com>
2016-03-27 15:44:06 +0900

    add use way
    + struct use way.

--- Rust/struct.rs
+++ Rust/struct.rs
@@ -5,8 +5,11 @@ struct Test {
 
 fn main() {
     let a = Test {a: 1, b: 0};
-    let mut b = Test{a:0, b: 1};
+    let mut b = Test{a: 0, b: 1};
+    let mut c = Test{a: 3, b: 0};
     b.b += 1;
+    c = Test{b: 2, .. c};
     println!("{}, {}", a.a, a.b);
     println!("{}, {}", b.a, b.b);
+    println!("{}, {}", c.a, c.b);
 }
