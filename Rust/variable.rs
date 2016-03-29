ddb6651be92acd90b9b25126d2e0cba0d07e8f46
Bahyun Lee <bahyun.lee@gmail.com>
2016-03-29 09:31:45 +0900

    &mut variable
    + mut variable.

--- Rust/variable.rs
+++ Rust/variable.rs
@@ -5,4 +5,7 @@ fn main() {
 	let e = 5; // e is not change.
 	let mut f = 6; // f able change.
 	println!("{}, {}, {}, {}, {}, {}", a, b, c, d, e, f);
+	let g = &mut f;
+	g += 1;
+	println!("{}", f);
 }
