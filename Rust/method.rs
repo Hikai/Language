4d7755a2e44f37ab418d39b8a805dd5cd7c7b40c
HiKi <bahyun.lee@gmail.com>
2016-03-30 14:31:33 +0900

    method
    + impl keyword, method.

---
+++ Rust/method.rs
@@ -0,0 +1,22 @@
+struct Char {
+    hp: i32,
+    mp: i32,
+    attk: i32,
+}
+
+impl Char {
+    fn get_attk(&self) -> i32 {
+        self.attk
+    }
+    fn get_hp(&self) -> i32 {
+        self.hp
+    }
+    fn get_mp(&self) -> i32 {
+        self.mp
+    }
+}
+
+fn main() {
+    let a = Char { hp: 100, mp: 50, attk: 10};
+    println!("{}, {}, {}", a.get_hp(), a.get_mp(), a.get_attk());
+}
