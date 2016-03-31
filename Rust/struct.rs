75132c9acf5c37e3add8c67e2f8febff12a62746
Bahyun Lee <bahyun.lee@gmail.com>
2016-04-01 00:37:36 +0900

    Test2 struct
    + generic.

--- Rust/struct.rs
+++ Rust/struct.rs
@@ -3,13 +3,22 @@ struct Test {
     b: i32,
 }
 
+struct Test2<T> {
+    x: T,
+    y: T,
+}
+
 fn main() {
     let a = Test {a: 1, b: 0};
-    let mut b = Test{a: 0, b: 1};
-    let mut c = Test{a: 3, b: 0};
+    let mut b = Test {a: 0, b: 1};
+    let mut c = Test {a: 3, b: 0};
+    let d = Test2 {x: 10, y: 20};
+    let e = Test2 {x: 10.1, y: 20.2};
     b.b += 1;
     c = Test{b: 2, .. c};
     println!("{}, {}", a.a, a.b);
     println!("{}, {}", b.a, b.b);
     println!("{}, {}", c.a, c.b);
+    println!("{}, {}", d.x, d.y);
+    println!("{}, {}", e.x, e.y);
 }
