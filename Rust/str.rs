1431712140bc0f551314af0bfc481d0ef1594333
Bahyun Lee <bahyun.lee@gmail.com>
2016-03-31 15:50:50 +0900

    string push_str
    + push_str exam.

--- Rust/str.rs
+++ Rust/str.rs
@@ -1,7 +1,10 @@
 fn main() {
     let str = "abcdef";
     let mut a = &str[1..3];
+    let mut b = "abc";
     println!("{}", a);
     a = &str[0..4];
-    println!("{}", a); 
+    println!("{}", a);
+    b.push_str("def");
+    println!("{}", b);
 }
