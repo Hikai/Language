f52b332930db9ff598e3d1692e5f2603201f2772
Bahyun Lee <bahyun.lee@gmail.com>
2016-04-04 00:08:57 +0900

    while
    + while example.

--- Rust/loop.rs
+++ Rust/loop.rs
@@ -2,4 +2,11 @@ fn main() {
     loop { // infinite loop
         println!("A");
     }
+    let mut sum = 0;
+    let mut i = 1;
+    while i <= 100 {
+        sum += i;
+        i += 1;
+    }
+    println!("Sum : {}", sum);
 }
