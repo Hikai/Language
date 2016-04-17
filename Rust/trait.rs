cbedf2e8a601f2aed33bfa3f91a213f7891dd2a3
Bahyun Lee <bahyun.lee@gmail.com>
2016-04-18 00:01:20 +0900

    trait example
    + rust trait example.

---
+++ Rust/trait.rs
@@ -0,0 +1,22 @@
+struct A {
+    x: i32,
+    y: i32,
+}
+
+trait Test {
+    fn multi(&self) -> i32;
+}
+
+impl Test for A {
+    fn multi(&self) -> i32 {
+        self.x * self.y
+    }
+}
+
+fn main() {
+    let a = A {
+        x: 123,
+        y: 321,
+    };
+    println!("Result : {}", a.multi());
+}
