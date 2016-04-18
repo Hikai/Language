e5b37b95924c0ac0a152024bcc1362afa2540c89
Bahyun Lee <bahyun.lee@gmail.com>
2016-04-19 00:09:51 +0900

    use struct example
    + add struct example.

--- Rust/struct.rs
+++ Rust/struct.rs
@@ -8,12 +8,15 @@ struct Test2<T> {
     y: T,
 }
 
+struct Test3(i32, f32);
+
 fn main() {
     let a = Test {a: 1, b: 0};
     let mut b = Test {a: 0, b: 1};
     let mut c = Test {a: 3, b: 0};
     let d = Test2 {x: 10, y: 20};
     let e = Test2 {x: 10.1, y: 20.2};
+    let Test3(integer, float) = Test3(123, 123.123123);
     b.b += 1;
     c = Test{b: 2, .. c};
     println!("{}, {}", a.a, a.b);
@@ -21,4 +24,5 @@ fn main() {
     println!("{}, {}", c.a, c.b);
     println!("{}, {}", d.x, d.y);
     println!("{}, {}", e.x, e.y);
+    println!("{}, {}", integer, float);
 }
