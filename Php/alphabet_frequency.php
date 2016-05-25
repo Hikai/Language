7a0fb01218dc305c42c85695647bd9ec00048973
Bahyun Lee <bahyun.lee@gmail.com>
2016-05-25 17:44:00 +0900

    frequency store array, ord
    add to frequency array and print ord.

--- Php/alphabet_frequency.php
+++ Php/alphabet_frequency.php
@@ -1,6 +1,15 @@
 <?php
 $str = "asdfasdfasdfasdf";
-for ($i = 0; $i < strlen($str); $i++) {
-    echo($str[$i]);
+$array_fre = Array();
+for ($i = 65; $i < 123; $i++) {
+    if ($i == 91) {
+        $i = 96;
+        continue;
+    }
+    $array_fre += Array(chr($i) => "");
 }
+for ($j = 0; $j < strlen($str); $j++) {
+    echo(ord($str[$j]));
+}
+var_dump($array_fre);
 ?>
