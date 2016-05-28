73acdec5bdf0f824b89caeb2eedeab419a696d01
Bahyun Lee <bahyun.lee@gmail.com>
2016-05-28 13:58:38 +0900

    add alphabet frequency function
    add alphabet frequency function argument : (string), return type : Array.

--- Php/lib.php
+++ Php/lib.php
@@ -119,4 +119,33 @@ function desc_array_sort($array)
     arsort($array);
     return $array;
 }
+function alphabet_frequency($str)
+{
+    if (!is_string($str)) {
+        return;
+    }
+    $array_fre = Array();
+    for ($i = 0; $i < strlen($str); $i++) {
+        if (ord($str[$i]) < 97) {
+            $str[$i] = chr(ord($str[$i]) + 32);
+        }
+        for ($j = 97; $j < 123; $j++) {
+            if (ord($str[$i]) == $j) {
+                if (!isset($array_fre[chr($j)])) {
+                    $array_fre += Array(chr($j) => 1);
+                }
+                else {
+                    $array_fre[chr($j)]++;
+                }
+            }
+        }
+    }
+    echo("\n");
+    $array_fre = desc_array_sort($array_fre);
+    $str_leng = strlen($str);
+    foreach ($array_fre as $key => $value) {
+        echo($key." => ".$value." (".number_format(($value / $str_leng * 100), 2)." %)\n");
+    }
+    return $array_fre;
+}
 ?>
