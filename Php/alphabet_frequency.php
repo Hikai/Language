b24f8fdfc215f7127f6a30d31a2cded113134de3
Bahyun Lee <bahyun.lee@gmail.com>
2016-05-30 00:00:58 +0900

    alphabet frequency example
    write html form code.

--- Php/alphabet_frequency.php
+++ Php/alphabet_frequency.php
@@ -1,6 +1,19 @@
+<!DOCTYPE html>
+<html>
+  <head>
+  </head>
+  <body>
+    <form action="" method="post">
+      <textarea name="str_exam"></textarea>
+    </form>
+  </body>
+</html>
 <?php
+if (!isset($_POST["str_exam"])) {
+	exit;
+}
 inclued("lib.php");
-$str = "aaaaabccccddABCD";
+$str = $_POST["str_exam"];
 $fre = alphabet_frequency($str);
-var_dump($fre);
+//var_dump($fre);
 ?>
