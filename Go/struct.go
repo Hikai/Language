35ad8ff2fd48e40d5871daac40dcad3c85f608c8
Bahyun Lee <bahyun.lee@gmail.com>
2016-06-04 00:26:33 +0900

    structure
    go language structure example.

---
+++ Go/struct.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Quad struct {
+	a int
+	b int
+}
+
+func main() {
+	a := Quad{2, 2}
+	fmt.Println(a.a, a.b)
+	a.a = 4
+	a.b = 6
+	fmt.Println(a.a, a.b)
+}
