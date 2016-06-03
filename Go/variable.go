8f96376649e2abef0bfe40a8e5f2f7e59f589d38
Bahyun Lee <bahyun.lee@gmail.com>
2016-06-04 00:26:18 +0900

    variable
    go language variable.

--- Go/variable.go
+++ Go/variable.go
@@ -5,10 +5,14 @@ import (
 )
 
 var a, b = 1, 2
+var (
+	c int32 = 3
+	d int64 = 1000
+)
 const CONST = 3.14
 
 func main() {
-	c, d := "c3", "d4"
-	fmt.Println(a, b, c, d)
-	fmt.Println(PI)
+	e, f := "c3", "d4"
+	fmt.Println(a, b, c, d, e, f)
+	fmt.Println(CONST)
 }
