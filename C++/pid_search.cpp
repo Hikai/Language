f941b39d7a2cba6809a57c32498a9072fddcef84
Bahyun Lee <bahyun.lee@gmail.com>
2016-06-27 06:09:55 +0900

    c++ search process name by pid
    C++ example.

---
+++ C++/pid_search.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <windows.h>
+
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+
+#define MAX_NAME 1024
+
+int main(void)
+{
+	char proc_name[MAX_NAME];
+	int pid = 0;
+	std::cout << "Enter pid: ";
+	std::cin >> pid;
+	if (HANDLE proc_handle = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, FALSE, pid)) {
+		if (GetModuleFileNameEx(proc_handle, 0, proc_name, MAX_NAME)) {
+		}
+		else {
+		}
+		CloseHandle(proc_handle);
+	}
+	return 0;
+}
