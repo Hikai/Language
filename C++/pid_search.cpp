5f6cf873c47dade40846712f6eb0b088f741edae
Bahyun Lee <bahyun.lee@gmail.com>
2016-06-28 21:36:38 +0900

    function definition
    C++ print process name function definition.

--- C++/pid_search.cpp
+++ C++/pid_search.cpp
@@ -7,20 +7,26 @@
 
 #define MAX_NAME 1024
 
+void print_process_name(DWORD pid)
+{
+	char proc_name[MAX_NAME] = "";
+	if ((HANDLE proc_handle = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, FALSE, pid)) != NULL) {
+		HMODULE hm_module;
+		DWORD dw_need;
+		if (EnumProcessModules(proc_handle, &hm_module, sizeof(hm_module), &dw_need)) {
+			GetModuleBaseName(proc_handle, hm_module, proc_name, sizeof(proc_name) / sizeof(char));
+		}
+	}
+	printf("Name : %s", proc_name);
+	CloseHandle(proc_handle)
+}
+
 int main(void)
 {
 	char proc_name[MAX_NAME];
 	int pid = 0;
 	std::cout << "Enter pid: ";
 	std::cin >> pid;
-	if (HANDLE proc_handle = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, FALSE, pid)) {
-		if (GetModuleFileNameEx(proc_handle, 0, proc_name, MAX_NAME)) {
-			std::cout << "Process name : " << proc_name << std::endl;
-		}
-		else {
-			std::cout << "Error" << std::endl;
-		}
-		CloseHandle(proc_handle);
-	}
+	print_process_name(pid);
 	return 0;
 }
