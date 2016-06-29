059f6c7c180636eb48dcbbca2ea4a249f5f4b2d3
Bahyun Lee <bahyun.lee@gmail.com>
2016-06-29 14:55:42 +0900

    method definitions
    constructor, set_, get_ method definition.

--- C++/template_base.cpp
+++ C++/template_base.cpp
@@ -11,7 +11,31 @@ public:
 	T get_data(void);
 };
 
+template <typename T>
+Exam_data<T>::Exam_data(T init_data)
+{
+	data = init_data;
+}
+
+template <typename T>
+void Exam_data<T>::set_data(T data)
+{
+	data = data;
+}
+
+template <typename T>
+T Exam_data<T>::get_data(void)
+{
+	return data;
+}
+
 int main()
 {
-
+	Exam_data<int> a(10);
+	std::cout << a.get_data() << std::endl;
+	a.set_data(10 + 20);
+	std::cout << a.get_data() << std::endl;
+	Exam_data<double> b(10.01);
+	std::cout << b.get_data() << std::endl;
+	return 0;
 }
