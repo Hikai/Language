50b7b05d669924eedb13ba8ece7d626d12d7ca12
Bahyun Lee <bahyun.lee@gmail.com>
2016-07-05 02:13:47 +0900

    requests library, BeautifulSoup findAll
    use library.

--- Python/git_commit_table.py
+++ Python/git_commit_table.py
@@ -5,19 +5,11 @@ Github commit table parsing.
 Todo:
 """
 from bs4 import BeautifulSoup
+import requests
 
 
-a = """
-<g transform="translate(0, 0)">
-          <rect class="day" width="11" height="11" y="0" fill="#eeeeee" data-count="0" data-date="2015-06-28"></rect>
-          <rect class="day" width="11" height="11" y="13" fill="#eeeeee" data-count="0" data-date="2015-06-29"></rect>
-          <rect class="day" width="11" height="11" y="26" fill="#eeeeee" data-count="0" data-date="2015-06-30"></rect>
-          <rect class="day" width="11" height="11" y="39" fill="#eeeeee" data-count="0" data-date="2015-07-01"></rect>
-          <rect class="day" width="11" height="11" y="52" fill="#eeeeee" data-count="0" data-date="2015-07-02"></rect>
-          <rect class="day" width="11" height="11" y="65" fill="#eeeeee" data-count="0" data-date="2015-07-03"></rect>
-          <rect class="day" width="11" height="11" y="78" fill="#eeeeee" data-count="0" data-date="2015-07-04"></rect>
-      </g>
-"""
-soup = BeautifulSoup(a)
-input_tag = soup.findAll(attrs={"data-count": "0"})
-print(input_tag)
+req = requests.get('https://www.github.com/hikillhikai/').text
+soup_git = BeautifulSoup(req)
+rects = soup_git.findAll("rect")
+for rect in rects:
+        print("{}: {}".format(rect["data-date"], rect["data-count"]))
