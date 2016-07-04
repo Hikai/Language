86f92a5e3eec181a44de97510fe6637d86884779
Bahyun Lee <bahyun.lee@gmail.com>
2016-07-04 15:21:52 +0900

    regular remove, use beautifulsoup
    parsing change.

--- Python/git_commit_table.py
+++ Python/git_commit_table.py
@@ -4,19 +4,20 @@ Github commit table parsing.
 
 Todo:
 """
-import re
-import requests
+from bs4 import BeautifulSoup
 
 
-def main():
-    """Main."""
-    user_id = 'hikillhikai'
-    req = requests.get('https://github.com/{}'.format(user_id)).text
-    reg = '<g transform=\"(?:.*)\">'\
-        '\n(.*)\n(.*)\n(.*)\n(.*)\n(.*)\n(.*)\n(.*)\n      </g>'
-    regular = re.findall(reg, req)
-    for line in regular:
-        print(line)
-
-if __name__ == "__main__":
-    main()
+a = """
+<g transform="translate(0, 0)">
+          <rect class="day" width="11" height="11" y="0" fill="#eeeeee" data-count="0" data-date="2015-06-28"></rect>
+          <rect class="day" width="11" height="11" y="13" fill="#eeeeee" data-count="0" data-date="2015-06-29"></rect>
+          <rect class="day" width="11" height="11" y="26" fill="#eeeeee" data-count="0" data-date="2015-06-30"></rect>
+          <rect class="day" width="11" height="11" y="39" fill="#eeeeee" data-count="0" data-date="2015-07-01"></rect>
+          <rect class="day" width="11" height="11" y="52" fill="#eeeeee" data-count="0" data-date="2015-07-02"></rect>
+          <rect class="day" width="11" height="11" y="65" fill="#eeeeee" data-count="0" data-date="2015-07-03"></rect>
+          <rect class="day" width="11" height="11" y="78" fill="#eeeeee" data-count="0" data-date="2015-07-04"></rect>
+      </g>
+"""
+soup = BeautifulSoup(a)
+input_tag = soup.findAll(attrs={"data-count": "0"})
+print(input_tag)
