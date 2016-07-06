fa515dd012bca9087cffa65c2a034a1cdecc50e7
Bahyun Lee <bahyun.lee@gmail.com>
2016-07-06 17:46:18 +0900

    print longest streak
    data_count list count.

--- Python/git_commit_table.py
+++ Python/git_commit_table.py
@@ -11,5 +11,14 @@ import requests
 req = requests.get('https://www.github.com/hikillhikai/').text
 soup_git = BeautifulSoup(req)
 rects = soup_git.findAll("rect")
+data_count = []
+# rect["data-date"], rect["data-count"]
 for rect in rects:
-        print("{}: {}".format(rect["data-date"], rect["data-count"]))
+    data_count.append(rect["data-count"])
+counter = 0
+for count in data_count:
+    if count == 0:
+        counter = 0
+        continue
+    counter += 1
+print("Longest streak: {}".format(counter))
