089c1c74761bd6eebf8edf20183733dd05e0986b
Bahyun Lee <bahyun.lee@gmail.com>
2016-07-08 22:40:53 +0900

    child right, left node variable
    add method, value in Node class.

--- Python/tree_node.py
+++ Python/tree_node.py
@@ -10,16 +10,21 @@ class Node:
     """Node class."""
 
     data = None
-    node_child = None
+    node_child_left = None
+    node_child_right = None
     node_parent = None
 
     def __init__(self, data):
         """Node class init method."""
         self.data = data
 
-    def set_child(self, node):
-        """Child node settings."""
-        self.node_child = node
+    def set_child_left(self, node):
+        """Child node left settings."""
+        self.node_child_left = node
+
+    def set_child_right(self, node):
+        """Child node right settings."""
+        self.node_child_left = node
 
     def set_parent(self, node):
         """Parent node settings."""
@@ -31,9 +36,10 @@ def main():
     node1 = Node(123)
     node2 = Node(321)
     node3 = Node(132)
+    node4 = Node(231)
     node2.set_parent(node1)
-    node2.set_child(node3)
-
+    node2.set_child_left(node3)
+    node2.set_child_right(node4)
 
 if __name__ == "__main__":
     main()
