0d9d776e64c72a81cd2cda98866836b803c9be65
Bahyun Lee <bahyun.lee@gmail.com>
2016-07-09 13:23:12 +0900

    Store class base, Node class del method
    Tree Store class base code.
    Node class __del__ method.

--- Python/tree_node.py
+++ Python/tree_node.py
@@ -6,6 +6,20 @@ To-do:
 """
 
 
+class Store:
+    """Tree store class."""
+
+    node_root = None
+
+    def __init__(self):
+        """Store class init method."""
+        pass
+
+    def __del__(self):
+        """Store class del method."""
+        pass
+
+
 class Node:
     """Node class."""
 
@@ -18,13 +32,24 @@ class Node:
         """Node class init method."""
         self.data = data
 
+    def __del__(self):
+        """Node class del method."""
+        self.data = None
+        if self.node_child_left is not None:
+            self.node_parent.node_child_left = self.node_child_left
+        if self.node_child_right is not None:
+            self.node_parent.noed_child_right = self.node_child_right
+        self.node_child_right.node_parent = self.node_parent
+        self.node_child_left.node_parent = self.node_parent
+        self.node_parent = None
+
     def set_child_left(self, node):
         """Child node left settings."""
         self.node_child_left = node
 
     def set_child_right(self, node):
         """Child node right settings."""
-        self.node_child_left = node
+        self.node_child_right = node
 
     def set_parent(self, node):
         """Parent node settings."""
@@ -40,6 +65,7 @@ def main():
     node2.set_parent(node1)
     node2.set_child_left(node3)
     node2.set_child_right(node4)
+    del node1
 
 if __name__ == "__main__":
     main()
