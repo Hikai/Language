ab9f187dced90686150c47f991b0aa221896736c
Bahyun Lee <bahyun.lee@gmail.com>
2016-07-10 07:25:36 +0900

    Gimpo airport real time info
    python parsing example.

---
+++ Python/gimpo_airport_arrive_parsing.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Gimpo Airport Arrive Information.
+
+To-do:
+"""
+# import re
+# import requests
+
+
+def main():
+    """Main."""
+    # reque_arr = requests.get('https://www.airport.co.kr/gimpo/extra/liveSchedule/liveScheduleList/layOut.do?langType=1&inoutType=OUT&cid=2015102611043202364&menuId=8')
+    # re_info = re.compile('<tr>\n\t\t\t\t\t\t\t\t\t\t<td><a(?:.*)>(.*)</a></td>\n\t\t\t\t\t\t\t\t\t\t<td>(.*)</td>\n\t\t\t\t\t\t\t\t\t\t<td>(.*)</td>\n\t\t\t\t\t\t\t\t\t\t<td>\n\t\t\t\t\t\t\t\t\t\t\t(.*)\n\n\t\t\t\t\t\t\t\t\t\t</td>\n(?:.*)\n\t\t\t\t\t\t\t\t\t\t<td>(.*)</td>\n\t\t\t\t\t\t\t\t\t\t<td>(.*)</td>\n(?:.*)\n\t\t\t\t\t\t\t\t\t\t<td>(.*)</td>\n\t\t\t\t\t\t\t\t\t\t<td>(.*)</td>')
+
+if __name__ == "__main__":
+    main()
