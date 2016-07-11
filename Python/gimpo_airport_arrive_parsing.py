e395b15305cbd8d12bcc6b282d42011fb103342a
Bahyun Lee <bahyun.lee@gmail.com>
2016-07-11 12:36:46 +0900

    use beautifulsoup, url update, regex update
    beautifulsoup class parsing, url update, regex change.

--- Python/gimpo_airport_arrive_parsing.py
+++ Python/gimpo_airport_arrive_parsing.py
@@ -4,14 +4,29 @@ Gimpo Airport Arrive Information.
 
 To-do:
 """
-# import re
-# import requests
+from bs4 import BeautifulSoup
+import re
+import requests
 
 
 def main():
     """Main."""
-    # reque_arr = requests.get('https://www.airport.co.kr/gimpo/extra/liveSchedule/liveScheduleList/layOut.do?langType=1&inoutType=OUT&cid=2015102611043202364&menuId=8')
-    # re_info = re.compile('<tr>\n\t\t\t\t\t\t\t\t\t\t<td><a(?:.*)>(.*)</a></td>\n\t\t\t\t\t\t\t\t\t\t<td>(.*)</td>\n\t\t\t\t\t\t\t\t\t\t<td>(.*)</td>\n\t\t\t\t\t\t\t\t\t\t<td>\n\t\t\t\t\t\t\t\t\t\t\t(.*)\n\n\t\t\t\t\t\t\t\t\t\t</td>\n(?:.*)\n\t\t\t\t\t\t\t\t\t\t<td>(.*)</td>\n\t\t\t\t\t\t\t\t\t\t<td>(.*)</td>\n(?:.*)\n\t\t\t\t\t\t\t\t\t\t<td>(.*)</td>\n\t\t\t\t\t\t\t\t\t\t<td>(.*)</td>')
+    url = 'https://www.airport.co.kr/gimpo/extra/liveSchedule/'\
+        'liveScheduleList/layOut.do?langType=1&inoutType=OUT&'\
+        'cid=2015102611043202364&menuId=8'
+    # 2016. 07. 11. 01:00 ~ 24:00
+    reque_arrive = requests.get(url)
+    reque_arrive.encoding = "unicode"
+    soup_reque = BeautifulSoup(reque_arrive.text, "lxml")
+    class_table = "table table2 table-hover text-center"
+    soup_table = soup_reque.findAll("table", {"class": class_table})
+    re_info = '<tr>\n<td><a (?:.*)>(.*)</a></td>\n<td>(.*)</td>\n<td>(.*)'\
+              '</td>\n<td>\n\n(.*)\n\n\n'\
+              '                                        </td>'\
+              '\n<td>(.*)</td>\n<td>(.*)</td>\n<td>(.*)</td>\n<td>(.*)</td>\n'\
+              '<td>\n<span (?:.*)>(.*)</span>'
+    regex = re.findall(re_info, str(soup_table))
+    print(regex)
 
 if __name__ == "__main__":
     main()
