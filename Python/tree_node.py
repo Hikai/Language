79593bc8026f8757958b5aef8ec7a81afd761aa3
Bahyun Lee <bahyun.lee@gmail.com>
2016-07-12 09:49:51 +0900

    add depth, node_child type change, . . .
    add depth value.
    node_child type change (list),
    __init__ method update,
    add add_child, get_child_data and get_parent_data method.

--- Python/tree_node.py
+++ Python/tree_node.py
@@ -24,47 +24,58 @@ class Node:
     """Node class."""
 
     data = None
-    node_child_left = None
-    node_child_right = None
+    depth = None
     node_parent = None
+    node_child = []
 
-    def __init__(self, data):
-        """Node class init method."""
+    def __init__(self, data, depth):
+        """
+        Node class init method.
+
+        data: Node data
+        depth: Node depth
+        ex) node_name = Node(data, depth)
+        """
         self.data = data
+        self.depth = depth
 
     def __del__(self):
         """Node class del method."""
         self.data = None
-        if self.node_child_left is not None:
-            self.node_parent.node_child_left = self.node_child_left
-        if self.node_child_right is not None:
-            self.node_parent.noed_child_right = self.node_child_right
-        self.node_child_right.node_parent = self.node_parent
-        self.node_child_left.node_parent = self.node_parent
         self.node_parent = None
-
-    def set_child_left(self, node):
-        """Child node left settings."""
-        self.node_child_left = node
-
-    def set_child_right(self, node):
-        """Child node right settings."""
-        self.node_child_right = node
 
     def set_parent(self, node):
         """Parent node settings."""
         self.node_parent = node
 
+    def add_child(self, *node):
+        """Child node add."""
+        for child in node:
+            self.node_child.append(child)
+
+    def get_child_data(self):
+        """Get child node."""
+        child_data = []
+        for node in self.node_child:
+            child_data.append(node.data)
+        return child_data
+
+    def get_parent_data(self):
+        """Get parent node."""
+        return self.node_parent.data
+
 
 def main():
     """Main."""
-    node1 = Node(123)
-    node2 = Node(321)
-    node3 = Node(132)
-    node4 = Node(231)
+    node1 = Node(1, 1)
+    node2 = Node(2, 2)
+    node3 = Node(3, 2)
     node2.set_parent(node1)
-    node2.set_child_left(node3)
-    node2.set_child_right(node4)
+    node3.set_parent(node1)
+    node1.add_child(node2, node3)
+    print(node1.get_child_data())
+    del node3
+    del node2
     del node1
 
 if __name__ == "__main__":
