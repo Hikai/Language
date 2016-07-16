cbd553fd3d71953db339d97606bb5599e9f0cd91
Bahyun Lee <bahyun.lee@gmail.com>
2016-07-16 18:01:46 +0900

    youtube link parsing
    clean up.

--- Python/youtube.py
+++ Python/youtube.py
@@ -13,10 +13,16 @@ def main():
     keyword = 'utaite'
     url = 'https://www.youtube.com/results?search_query={}'.format(keyword)
     soup_ytb = BeautifulSoup(requests.get(url).text, "lxml")
-    list_h3 = soup_ytb.findAll("h3", {"class": "yt-lockup-title"})
-    for item in list_h3:
-        print(item.text)
-        print(item['class'])
+    tag_h3 = soup_ytb.findAll("h3", {"class": "yt-lockup-title"})
+    soup_a = BeautifulSoup(str(tag_h3), "lxml")
+    tag_a = soup_a.findAll("a")
+    list_href = []
+    list_title = []
+    for each in tag_a:
+        list_href.append(each['href'])
+    for item in tag_h3:
+        list_title.append(item.text)
+    print(list_href, list_title)
 
 
 if __name__ == "__main__":
