3331ba8bc636a482e54595e4c74ac7a90c88ea20
Bahyun Lee <bahyun.lee@gmail.com>
2016-07-19 21:34:08 +0900

    method abstract
    push, pop method.

---
+++ C++/stack.cpp
@@ -0,0 +1,16 @@
+#include "stack.h"
+
+int * Stack::get_data(void)
+{
+    return store;
+}
+
+void Stack::push(int data)
+{
+    // store.append(data)
+}
+
+void Stack::pop(void)
+{
+    // store.pop();
+}
