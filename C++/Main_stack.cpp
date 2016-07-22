5dcc5bb7a3bd2fedeb120b68f7a37a822b53ec7e
Bahyun Lee <bahyun.lee@gmail.com>
2016-07-22 09:22:28 +0900

    stack class main example
    push, get_data, pop method example.

--- C++/Main_stack.cpp
+++ C++/Main_stack.cpp
@@ -3,5 +3,13 @@
 int main(void)
 {
 	Stack a = new Stack();
+	a.push(123);
+	a.push(321);
+	a.push(132);
+	std::cout << a.get_data() << endl;
+	a.pop();
+	a.pop();
+	a.pop();
 	delete a;
+	return 0;
 }
