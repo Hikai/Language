f4271edee35d90c20b600270015bc25d12255d4a
Bahyun Lee <bahyun.lee@gmail.com>
2016-07-25 20:26:55 +0900

    party_list

---
+++ Php/tmp/party_list.php
@@ -0,0 +1,16 @@
+<?php
+if (!isset($_SESSION)) { // Session check.
+    session_start();
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Party list</title>
+</head>
+<body>
+    <p>[Party list]</p>
+    <br />
+    <p onclick="">Create party</p>
+</body>
+</html>
