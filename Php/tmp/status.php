248be833663224a8dc8c7d744dc70244cc7b1da8
Bahyun Lee <bahyun.lee@gmail.com>
2016-07-25 20:27:02 +0900

    status.php

---
+++ Php/tmp/status.php
@@ -0,0 +1,14 @@
+<?php
+if (!isset($_SESSION)) { // Session check.
+    session_start();
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Character status</title>
+</head>
+<body>
+    <p>[Status]</p>
+</body>
+</html>
