65d4ce40a65eae2f6ec4718aeede766c8b734622
Bahyun Lee <bahyun.lee@gmail.com>
2016-07-26 15:50:08 +0900

    Player class base

---
+++ Php/tmp/lib.php
@@ -0,0 +1,12 @@
+<?php
+class Player {
+    private $attack = 0, $avoid = 0, $critical = 0, $defence = 0, $hp = 0;
+    static private $cost = 10;
+    function __construct() {
+
+    }
+    function __destruct() {
+        
+    }
+}
+?>
