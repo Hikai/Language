ecf75042cd3cfd2beca1ba72932cf7b85f3939c9
Bahyun Lee <bahyun.lee@gmail.com>
2016-07-31 12:13:26 +0900

    party list

--- Php/tmp/party_list.php
+++ Php/tmp/party_list.php
@@ -12,7 +12,10 @@ if (!isset($_SESSION)) { // Session check.
     <p>[Party list]</p>
     <br />
 <?
-// List.
+include("lib.php");
+foreach($arr_party as $key_party) {
+    echo("<p onclick=\"\">".$key_party."</p>");
+}
 ?>
     <br />
     <p onclick="">Create party</p>
