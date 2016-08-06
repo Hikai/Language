18cbf3831f387c3f951fe88ab9ae08c9767608ba
Bahyun Lee <bahyun.lee@gmail.com>
2016-08-06 13:30:24 +0900

    example change

--- C++/Main_stack.cpp
+++ C++/Main_stack.cpp
@@ -2,14 +2,9 @@
 
 int main(void)
 {
-	Stack a = new Stack();
-	a.push(123);
-	a.push(321);
-	a.push(132);
-	std::cout << a.get_data() << endl;
+	Stack<int> a;
+	a.push(123123);
+	std::cout << a.get_data() << std::endl;
 	a.pop();
-	a.pop();
-	a.pop();
-	delete a;
 	return 0;
 }
