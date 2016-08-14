1e57fa0d150c566f7c64d38cd6997d67f411b6a7
Bahyun Lee <bahyun.lee@gmail.com>
2016-08-15 01:21:40 +0900

    to_hex return type change, example source edit

--- Rust/to_hex.rs
+++ Rust/to_hex.rs
@@ -1,6 +1,7 @@
-fn to_hex (chr: u32) -> Vec<String> {
+fn to_hex (chr: u32) -> String {
     let mut hex_operand = chr;
-    let mut vec = Vec::new();
+    let mut vec_hex = Vec::new();
+    let mut str_result = String::new();
 
     if chr >= 16 {
         loop {
@@ -9,51 +10,51 @@ fn to_hex (chr: u32) -> Vec<String> {
                 break;
             }
             
-            vec.push(alphabet_match(hex_operand % 16));
+            vec_hex.push(alphabet_match_lower(hex_operand % 16));
             
             hex_operand /= 16;
             if hex_operand > 15 {
                 continue;
             }
             else {
-                vec.push(alphabet_match(hex_operand));
+                vec_hex.push(alphabet_match_lower(hex_operand));
                 break;
             }
 
         }
     }
     else {
-        vec.push(alphabet_match(hex_operand));
+        vec_hex.push(alphabet_match_lower(hex_operand));
     }
 
-    vec.reverse();
+    vec_hex.reverse();
     
-    vec
+    for item in vec_hex {
+        str_result.push_str(item.as_ref());
+    }
+    
+    str_result
 }
 
-fn alphabet_match (hex: u32) -> String {
-    let alphabet;
-    
-    match hex {
-        10 => alphabet = "a".to_string(),
-        11 => alphabet = "b".to_string(),
-        12 => alphabet = "c".to_string(),
-        13 => alphabet = "d".to_string(),
-        14 => alphabet = "e".to_string(),
-        15 => alphabet = "f".to_string(),
-        _ => alphabet = hex.to_string(),
+fn alphabet_match_lower (hex: u32) -> String {
+    let alphabet = match hex {
+        10 => "a".to_string(),
+        11 => "b".to_string(),
+        12 => "c".to_string(),
+        13 => "d".to_string(),
+        14 => "e".to_string(),
+        15 => "f".to_string(),
+        _ => hex.to_string(),
     };
 
     alphabet
 }
 
 fn main() {
-    let a = 63;
-    let vec = to_hex(a as u32);
-    
-    print!("{} hex -> ", a);
-    
-    for v in vec {
-        print!("{}", v);
+    let a = "abcde";
+    let mut vec_hex = Vec::new();
+    for c in a.chars() {
+        print!("{} ", to_hex(c as u32));
+        vec_hex.push(c as u32);
     }
 }
