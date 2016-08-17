04869ae77d4465c16d5fd325973c26aabc83eef9
Bahyun Lee <bahyun.lee@gmail.com>
2016-08-18 01:34:32 +0900

    remove convert hex

--- Rust/binary_open.rs
+++ Rust/binary_open.rs
@@ -4,20 +4,6 @@ use std::fs::File;
 use std::io::Read;
 use std::path::Path;
 
-fn alphabet_match_upper (hex: u8) -> String {
-    let alphabet = match hex {
-        10 => "A".to_string(),
-        11 => "B".to_string(),
-        12 => "C".to_string(),
-        13 => "D".to_string(),
-        14 => "E".to_string(),
-        15 => "F".to_string(),
-        _ => hex.to_string(),
-    };
-
-    return alphabet;
-}
-
 fn file_read(ref_path: &OsStr) -> [u8; 1024] {
     let mut byte_store = [0u8; 1024];
 
@@ -28,39 +14,10 @@ fn file_read(ref_path: &OsStr) -> [u8; 1024] {
     return byte_store;
 }
 
-// thruput
-// fn str_to_ref_osstr<'a>(arg: &'a String) -> &'a OsStr {
-//     let ref_str: &str = arg.as_ref();
-//     let ref_osstr: &OsStr = OsStr::new(ref_str);
+fn ref_str_to_ref_osstr(arg_filename: &str) -> &OsStr {
+    let ref_osstr: &OsStr = OsStr::new(arg_filename);
 
-//     return ref_osstr;
-// }
-
-fn to_hex (chr: u8) -> String {
-    let mut hex_operand = chr;
-    let mut vec_hex = Vec::new();
-    let mut str_result = String::new();
-
-    loop {
-        vec_hex.push(alphabet_match_upper(hex_operand % 16));
-        hex_operand /= 16;
-
-        if hex_operand > 15 {
-            continue;
-        }
-        else {
-            vec_hex.push(alphabet_match_upper(hex_operand));
-            break;
-        }
-    }
-
-    vec_hex.reverse();
-    
-    for item in vec_hex {
-        str_result.push_str(item.as_ref());
-    }
-    
-    return str_result;
+    return ref_osstr;
 }
 
 fn main() {
@@ -71,14 +28,13 @@ fn main() {
         std::process::exit(1);
     }
 
-    let ref_str: &str = args[1].as_ref();
-    let ref_osstr: &OsStr = OsStr::new(ref_str);
+    let ref_osstr: &OsStr = ref_str_to_ref_osstr(&args[1]);
 
     let store = file_read(ref_osstr);
 
     let mut count = 0;
     for byte in store.iter() {
-        print!("{:3} ", to_hex(*byte));
+        print!("{:3} ", byte);
         if count >= 7 {
             println!("");
 
