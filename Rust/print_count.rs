92f3a6e6ba381e43d9bd9a3e857ae79f26b83134
Bahyun Lee <bahyun.lee@gmail.com>
2016-08-18 16:23:11 +0900

    decreament count function.

--- Rust/print_count.rs
+++ Rust/print_count.rs
@@ -1,13 +1,22 @@
 use std::time::Duration;
 use std::thread;
 
-fn print_count(start: u32, end: u32, increament: u32) {
+fn incre_print_count(start: u32, end: u32, increment: u32) {
     for i in start..end {
         print!("\r{}", i);
-        i += increament;
+        i += increment;
+    }
+}
+
+fn decre_print_count(start: u32, end: u32, decrement: u32) {
+    for i in (start..end).rev() {
+        print!("\r{}", i);
+        i -= decrement;
     }
 }
 
 fn main() {
-    print_count(0, 1000000);
+    incre_print_count(0, 1000000, 2);
+    println!("");
+    decre_print_count(0, 1000000, 2);
 }
