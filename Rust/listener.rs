d869063a5753a17f8508158b051a5809533efbe6
Bahyun Lee <bahyun.lee@gmail.com>
2016-08-23 00:52:09 +0900

    handle_client function

--- Rust/listener.rs
+++ Rust/listener.rs
@@ -1,13 +1,19 @@
-use std::net::TcpListener;
+use std::net::{TcpListener, TcpStream};
+use std::thread;
+
+fn handle_client(stream: TcpStream) {
+    println!("Peer: {:?}, Local: {:?}", stream.peer_addr(), stream.local_addr());
+}
 
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:7578").unwrap();
+    let listener = TcpListener::bind("127.0.0.1:80").unwrap();
     for stream in listener.incoming() {
         match stream {
             Err(log) => println!("{:?}", log),
             Ok(stream) => {
-                println!("connect");
-                println!("{:?}\n", stream);
+                thread::spawn(move || {
+                    handle_client(stream);
+                });
             },
         };
     }
