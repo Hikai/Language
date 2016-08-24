6858db244c401b4a7170a7c492c4ebecc1ceb324
Bahyun Lee <bahyun.lee@gmail.com>
2016-08-24 13:15:13 +0900

    strem read, write example

--- Rust/listener.rs
+++ Rust/listener.rs
@@ -1,8 +1,18 @@
+use std::io::Read;
+use std::io::Write;
 use std::net::{TcpListener, TcpStream};
 use std::thread;
 
-fn handle_client(stream: TcpStream) {
-    println!("Peer: {:?}, Local: {:?}", stream.peer_addr(), stream.local_addr());
+fn handle_client(mut stream: TcpStream) {
+    let mut buf = [0u8, 255];
+    loop {
+        let buf_read = stream.read(&mut buf).unwrap();
+        if buf_read == 0 {
+            break;
+        }
+        println!("{:?}", buf);
+        stream.write_all(&buf).unwrap();
+    }
 }
 
 fn main() {
@@ -10,7 +20,8 @@ fn main() {
     for stream in listener.incoming() {
         match stream {
             Err(log) => println!("{:?}", log),
-            Ok(stream) => {
+            Ok(mut stream) => {
+                stream.write_all(b"Connected\n\n").unwrap();
                 thread::spawn(move || {
                     handle_client(stream);
                 });
