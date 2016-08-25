680c5a8fe4d588b2ab060713c85419c8c560b9eb
Bahyun Lee <bahyun.lee@gmail.com>
2016-08-25 14:29:35 +0900

    rust client data write example

--- Rust/stream.rs
+++ Rust/stream.rs
@@ -1,6 +1,15 @@
+use std::io::prelude::*;
 use std::net::TcpStream;
 
 fn main() {
-    let stream = TcpStream::connect("127.0.0.1:80").unwrap();
-    println!("{:?}", stream);
+    let mut stream = TcpStream::connect("127.0.0.1:80").unwrap();
+    let _ = stream.write_all(b"abcdef").unwrap();
+    let mut buf = [0u8; 128];
+    let _ = stream.read(&mut buf);
+
+    for i in buf.iter() {
+        print!("{}", *i as char);
+    }
+
+    drop(stream);
 }
