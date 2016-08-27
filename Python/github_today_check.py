e5517d69bc0f9cbda400d800228c48278d65fc95
Bahyun Lee <bahyun.lee@gmail.com>
2016-08-27 12:49:23 +0900

    github today commit check

---
+++ Python/github_today_check.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Github commit table parsing.
+
+Todo:
+"""
+from bs4 import BeautifulSoup
+from time import localtime, strftime
+import re
+import requests
+
+
+def get_today():
+    """Return today date."""
+    return strftime("%Y-%m-%d", localtime())
+
+
+def main(user_id):
+    """Main function."""
+    req = requests.get('https://www.github.com/{}/'.format(user_id)).text
+    soup_git = BeautifulSoup(req, "lxml")
+    rect = soup_git.findAll("rect", {"data-date": get_today()})
+    re_data = "data-count=\"(.*)\""
+    regex = re.findall(re_data, str(rect))
+    print(str(regex)[2])
+
+if __name__ == "__main__":
+    main("hikillhikai")
