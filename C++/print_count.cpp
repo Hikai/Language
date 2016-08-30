bc51b08a0653f1ccbd9f2194be53ce325a2689d0
Bahyun Lee <bahyun.lee@gmail.com>
2016-08-30 22:05:53 +0900

    count function delay argument

--- C++/print_count.cpp
+++ C++/print_count.cpp
@@ -1,18 +1,20 @@
 #include <iostream>
+#include <windows.h>
 
-void print_count(int init, int increament, int end)
+void print_count(int init, int increament, int end, int delay)
 {
     int loop = 0;
     
     for (loop = init; loop <= end; loop += increament) {
         printf("\r%d", loop);
+        Sleep(delay);
     }
     printf("\n");
 }
 
 int main(void)
 {
-    print_count(0, 2, 99999999);
+    print_count(0, 2, 99999999, 1000);
 
     return 0;
 }
