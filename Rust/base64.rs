33a1d82db5b1294adb0877bdbc7872d723f10d42
Bahyun Lee <bahyun.lee@gmail.com>
2016-09-03 14:52:42 +0900

    base64 base code

---
+++ Rust/base64.rs
@@ -0,0 +1,6 @@
+fn main() {
+    let table_base = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
+    let plain = "abc"
+    let bytes = plain.len() * 8 / 6;
+    let padding = plain.len() * 8 % 6;
+}
