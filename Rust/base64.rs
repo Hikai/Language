e70a27a4ddfd3c7768a69c80f7210fb435d5d6d8
Bahyun Lee <bahyun.lee@gmail.com>
2016-09-04 01:48:49 +0900

    uncleared issue

--- Rust/base64.rs
+++ Rust/base64.rs
@@ -1,6 +1,22 @@
 fn main() {
-    let table_base = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
-    let plain = "abc"
-    let bytes = plain.len() * 8 / 6;
+    // let table_base = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+    let plain = "abc";
+    // let bytes = plain.len() * 8 / 6;
     let padding = plain.len() * 8 % 6;
+    if padding != 0 {
+        // padding process.
+    }
+    let mut result = 0;
+    let mut index = plain.len() - 1;
+    let mut count = 0;
+    while index >= 0 {
+        result += (256u32.pow(count) * plain.as_bytes()[index] as u32);
+        // println!("{}", plain.as_bytes()[index]);
+        if index == 0 {
+            break;
+        }
+        index -= 1;
+        count += 1;
+    }
+    println!("{}", result);
 }
