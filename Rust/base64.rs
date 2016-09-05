c712e61371d749fa57cb27d0de5b6a375bd20c73
Bahyun Lee <bahyun.lee@gmail.com>
2016-09-05 10:36:27 +0900

    base64 code

--- Rust/base64.rs
+++ Rust/base64.rs
@@ -1,22 +1,40 @@
 fn main() {
-    // let table_base = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
     let plain = "abc";
-    // let bytes = plain.len() * 8 / 6;
-    let padding = plain.len() * 8 % 6;
-    if padding != 0 {
-        // padding process.
-    }
-    let mut result = 0;
+    let table_base = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
     let mut index = plain.len() - 1;
-    let mut count = 0;
-    while index >= 0 {
-        result += (256u32.pow(count) * plain.as_bytes()[index] as u32);
-        // println!("{}", plain.as_bytes()[index]);
-        if index == 0 {
+    let mut count: usize = 0;
+    let mut result = 0;
+    let mut vec_result = Vec::new();
+    let byte_chrs: usize = plain.len() * 8 / 6;
+    let byte_rest = plain.len() * 8 % 6;
+
+
+    while index != 0 {
+        result += 256u32.pow(count as u32) * plain.as_bytes()[index] as u32;
+        if index == 1 {
+            index -= 1;
+            count += 1;
+            result += 256u32.pow(count as u32) * plain.as_bytes()[index] as u32;
+
             break;
         }
-        index -= 1;
+        else {
+            index -= 1;
+            count += 1;
+        }
+    }
+    
+    count = 0;
+    drop(index);
+    
+    while count != byte_chrs {
+        vec_result.push(result / (64u32.pow(count as u32)) % 64);
         count += 1;
     }
-    println!("{}", result);
+    vec_result.reverse();
+
+    for v in vec_result {
+        count = v as usize;
+        println!("{}, {}", v, table_base.as_bytes()[count] as char);
+    }
 }
