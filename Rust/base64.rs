72fc65552bc02ea09f8012ed6a9a4f088bdf7f5c
Bahyun Lee <bahyun.lee@gmail.com>
2016-09-06 14:39:13 +0900

    functional, code re-write

--- Rust/base64.rs
+++ Rust/base64.rs
@@ -1,40 +1,52 @@
-fn main() {
-    let plain = "abc";
-    let table_base = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
-    let mut index = plain.len() - 1;
-    let mut count: usize = 0;
-    let mut result = 0;
-    let mut vec_result = Vec::new();
-    let byte_chrs: usize = plain.len() * 8 / 6;
-    let byte_rest = plain.len() * 8 % 6;
+fn matching_table(base64_code: usize) -> char {
+    let table_base64 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 
+    table_base64.as_bytes()[base64_code] as char
+}
 
-    while index != 0 {
-        result += 256u32.pow(count as u32) * plain.as_bytes()[index] as u32;
-        if index == 1 {
-            index -= 1;
-            count += 1;
-            result += 256u32.pow(count as u32) * plain.as_bytes()[index] as u32;
+fn three_bytes_encode(str_plain: &str) -> String {
+    let rev_plain = str_plain.chars().rev().collect::<String>();
+    let result_256 = to_256(&rev_plain);
+    let result_64 = to_64(&rev_plain, result_256);
+    let mut vec_chr = Vec::new();
 
-            break;
-        }
-        else {
-            index -= 1;
-            count += 1;
-        }
+    for chr in result_64 {
+        vec_chr.push(matching_table(chr as usize));
     }
-    
-    count = 0;
-    drop(index);
-    
+
+    let s = vec_chr.into_iter().collect();
+    s
+}
+
+fn to_256(str_plain: &str) -> u32 {
+    let mut count = 0;
+    let mut index = 0;
+    let mut result = 0;
+
+    while index <= str_plain.len() - 1 {
+        result += 256u32.pow(count as u32) * str_plain.as_bytes()[index] as u32;
+        count += 1;
+        index += 1;
+    }
+
+    result
+}
+
+fn to_64(str_plain: &str, value_256: u32) -> Vec<u32> {
+    let byte_chrs = str_plain.len() * 8 / 6;
+    let mut count = 0;
+    let mut vec_result = Vec::new();
+
     while count != byte_chrs {
-        vec_result.push(result / (64u32.pow(count as u32)) % 64);
+        vec_result.push(value_256 / 64u32.pow(count as u32) % 64);
         count += 1;
     }
-    vec_result.reverse();
 
-    for v in vec_result {
-        count = v as usize;
-        println!("{}, {}", v, table_base.as_bytes()[count] as char);
-    }
+    vec_result.reverse();
+    vec_result
+}
+
+fn main() {
+    let plain = "abc";
+    println!("{} -> {}", plain, three_bytes_encode(&plain));
 }
