fbf4772032d443fbde3134165084f7a7c1ee9f68
Bahyun Lee <bahyun.lee@gmail.com>
2016-09-09 14:17:41 +0900

    twitter api example

---
+++ Python/twitter_user_followers.py
@@ -0,0 +1,38 @@
+# -* -coding: utf-8 -*-
+"""
+Twitter api example.
+
+To-do :
+"""
+
+from twitter import OAuth, Twitter
+import ujson as json
+
+
+ACCESS_TOKEN = '1660682539-nwLJu95X57ePGSJNn7wBSyzFzYCb6vVfrsbZyvo'
+ACCESS_SECRET = 'mWzTNLDPjvJrQP2JCTzPdfHATxwU5ZDS8kDDALVJvRDFk'
+CONSUMER_KEY = 'Gch07t42WL0K55Ps2IixwZ2Ui'
+CONSUMER_SECRET = '8JLzTjKGRc9MzrLLXvs02L4op5RXVW9dmfDqyT9WjJmXywQhFF'
+
+
+def get_dumps(json_api):
+    """Return json.loads."""
+    return json.dumps(json_api)
+
+
+def get_loads(json_dumps):
+    """Return json.loads."""
+    return json.loads(json_dumps)
+
+
+def main():
+    """Main."""
+    oauth = OAuth(ACCESS_TOKEN, ACCESS_SECRET, CONSUMER_KEY, CONSUMER_SECRET)
+    twitt = Twitter(auth=oauth)
+    list_foler = get_dumps(twitt.followers.list(screen_name="applemelon1101"))
+    # list_foloi = json.dumps(twitt.friends.list(screen_name="applemelon1101"))
+    for user in get_loads(list_foler)['users']:
+        print(user)
+
+if __name__ == "__main__":
+    main()
