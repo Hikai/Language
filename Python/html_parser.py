a47997bb2e6fcfa83ed49baeb3439cafc799b2ff
Bahyun Lee <bahyun.lee@gmail.com>
2016-09-21 14:05:15 +0900

    class write

--- Python/html_parser.py
+++ Python/html_parser.py
@@ -7,43 +7,45 @@ To-do:
 import re
 
 
+class Selector():
+    """HTML Selector."""
 
-def find_all(value, html):
+    html = ""
 
+    def __init__(self, str_html):
+        """Selector class constructor."""
+        self.html = str_html
 
+    def find_class(self, value):
+        """Print class attribute tag."""
+        value = "class=\"{}\"".format(value)
+        for tag in get_all_tag_list(self.html):
+            if value in tag:
+                print(tag)
+                break
 
-def find_class(value, html):
-    """Print class tag."""
-    value = "class=\"{}\"".format(value)
-    for tag in get_all_tag_list(html):
-        if value in tag:
-            print(tag)
-            break
+    def find_id(self, value):
+        """Print id tag."""
+        value = "id=\"{}\"".format(value)
+        for tag in get_all_tag_list(self.html):
+            if value in tag:
+                print(tag)
+                break
 
-
-def find_id(value, html):
-    """Print id tag."""
-    value = "id=\"{}\"".format(value)
-    for tag in get_all_tag_list(html):
-        if value in tag:
-            print(tag)
-            break
+    def print_selector(self, selector):
+        """Selector quarter."""
+        value = selector[1:]
+        selector = selector[0]
+        quarter = {
+            ".": self.find_class,
+            "#": self.find_id,
+        }
+        quarter.get(selector)(value)
 
 
 def get_all_tag_list(str_html):
     """Return open, close, attribute list."""
     return re.findall('(<(?:|/)[a-z]+>|<(?:|/)[a-z]+(?:.*)>)', str_html)
-
-
-def selector(select, html):
-    """Selector quarter."""
-    value = select[1:]
-    select = select[0]
-    quarter = {
-        ".": find_class,
-        "#": find_id,
-    }
-    quarter.get(select)(value, html)
 
 
 def main():
@@ -56,7 +58,8 @@ def main():
 <body>
 </body>
 </html>"""
-    selector("#hikai", str_html)
+    selector = Selector(str_html)
+    selector.print_selector("#hiki")
 
 if __name__ == "__main__":
     main()
