cc53f7de35e9c8df9271ec8f8095135fe1245ecd
Bahyun Lee <bahyun.lee@gmail.com>
2016-09-25 15:56:24 +0900

    funtional

--- Python/bns_item.py
+++ Python/bns_item.py
@@ -4,20 +4,32 @@ Blade & Soul Character item.
 To-do:
 """
 from bs4 import BeautifulSoup
+import re
 import requests
 
-cookies = {
-# cookiesssssss
+COOKIES = {
+# cookies
 }
 
-headers = {
-# headersssssss
+HEADERS = {
+# headers
 }
 
-re_name = re.compile('<th class="name"><span(?:.*)>(.*)</span></th>')
-url = 'http://bns.plaync.com/bs/character/profile/Hikai?s=3'
-req = requests.get(url, headers=headers, cookies=cookies)
-soup = BeautifulSoup(req.text, "lxml")
-item_name = soup.findAll("th", {"class": "name"})
-for item in item_name:
-    print(re_name.findall(str(item))[0])
+RE_NAME = re.compile('<th class="name"><span(?:.*)>(.*)</span></th>')
+LIST_URL = ['http://bns.plaync.com/bs/character/profile/Hikai?s=3',
+'http://bns.plaync.com/bs/character/profile/%EB%A9%B8%EC%B2%9C%EB%A7%88%EC%8B%A0?s=20',
+'http://bns.plaync.com/bs/character/profile/RisK?s=7']
+
+
+def main():
+    """Main."""
+    for link in LIST_URL:
+        req = requests.get(link, headers=HEADERS, cookies=COOKIES)
+        soup = BeautifulSoup(req.text, "lxml")
+        item_name = soup.findAll("th", {"class": "name"})
+        for item in item_name:
+            print(RE_NAME.findall(str(item))[0])
+        print("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
+
+if __name__ == "__main__":
+    main()
