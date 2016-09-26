52c859a2fa1bf8b363790c1cf14c4e4b944b0cbb
Bahyun Lee <bahyun.lee@gmail.com>
2016-09-27 02:51:17 +0900

    functional

--- Python/bns_stat.py
+++ Python/bns_stat.py
@@ -18,10 +18,17 @@ HEADERS = {
 
 RE_STAT = re.compile('<dt class="stat-title">(.*)<span(?:.*)>(.*)</span></dt>')
 
-url = 'http://bns.plaync.com/bs/character/profile/Hikai?s=3'
-req = requests.get(url, headers=HEADERS, cookies=COOKIES)
-soup = BeautifulSoup(req.text, "lxml")
-stat_name = soup.findAll("dt", {"class": "stat-title"})
-for stat in stat_name:
-    tpl_stat = RE_STAT.findall(str(stat))[0]
-    print("{}: {}".format(tpl_stat[0], tpl_stat[1]))
+
+def main():
+    """Main."""
+    url = 'http://bns.plaync.com/bs/character/profile/Hikai?s=3'
+    req = requests.get(url, headers=HEADERS, cookies=COOKIES)
+    soup = BeautifulSoup(req.text, "lxml")
+    stat_name = soup.findAll("dt", {"class": "stat-title"})
+    for stat in stat_name:
+        tpl_stat = RE_STAT.findall(str(stat))[0]
+        print("{}: {}".format(tpl_stat[0], tpl_stat[1]))
+
+
+if __name__ == "__main__":
+    main()
