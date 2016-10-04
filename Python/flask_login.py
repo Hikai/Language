9856f09cfc8f8376cf7fa7afc7671f9b4ea92c73
Bahyun Lee <bahyun.lee@gmail.com>
2016-10-04 10:30:29 +0900

    route resettings, index, favicon, login

--- Python/flask_login.py
+++ Python/flask_login.py
@@ -3,18 +3,32 @@ Flask login.
 
 example.
 """
-from flask import Flask, render_template, redirect, request
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
-@app.route('/', methods=['POST'])
-def login():
+@app.route('/')
+def index():
+    """Index page rendering."""
+    return render_template("index.html")
+
+
+@app.route("/favicon.ico")
+def work_favicon():
+    """Favicon processing."""
+    return
+
+
+@app.route("/login", methods=["POST"])
+def login(name=None, passwd=None):
     """Root login page."""
-    if request.method == 'POST':
-        if request.form['name'] == "" or request.form['passwd'] == "":
+    if request.method == "POST":
+        if request.form["name"] == "" or request.form["passwd"] == "":
             return render_template("index.html")
         else:
-            return redirect("home.html")
+            val_name = request.form["name"]
+            val_pw = request.form["passwd"]
+            return render_template("home.html", name=val_name, passwd=val_pw)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", port="7578")
