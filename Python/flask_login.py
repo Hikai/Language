e7663da6806f37fb47b937a66f2db675e12ef242
Bahyun Lee <bahyun.lee@gmail.com>
2016-10-08 18:58:33 +0900

    session, logout

--- Python/flask_login.py
+++ Python/flask_login.py
@@ -3,8 +3,15 @@ Flask login.
 
 example.
 """
-from flask import Flask, render_template, request
+from flask import (
+    Flask, redirect, render_template, request, session, url_for
+)
 app = Flask(__name__)
+
+
+def pop_session(val_session):
+    """Session value pop."""
+    session.pop(val_session, None)
 
 
 @app.route('/')
@@ -28,7 +35,17 @@ def login(name=None, passwd=None):
         else:
             val_name = request.form["name"]
             val_pw = request.form["passwd"]
+            session["name"] = val_name
+            session["passwd"] = val_pw
             return render_template("home.html", name=val_name, passwd=val_pw)
+
+
+@app.route("/logout")
+def logout():
+    """Logout page."""
+    pop_session("name")
+    pop_session("passwd")
+    return redirect(url_for("index"))
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=7578)
