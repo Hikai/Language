340520b6e63b880ae8c364bb4e449fa512254b14
Bahyun Lee <bahyun.lee@gmail.com>
2016-10-23 22:28:12 +0900

    add file exists

--- Python/rss_parse.py
+++ Python/rss_parse.py
@@ -3,14 +3,18 @@ Python nyaa page rss parse.
 
 .
 """
+from os.path import exists
 from xml.etree import ElementTree
 import requests
 
 
 file_name = "rss.xml"
-xml = requests.get("https://www.nyaa.se/?page=rss").text
-with open(file_name, "wb") as file_xml:
-    file_xml.write(xml.encode("utf-8"))
+if exists(file_name):
+    pass
+else:
+    xml = requests.get("https://www.nyaa.se/?page=rss").text
+    with open(file_name, "wb") as file_xml:
+        file_xml.write(xml.encode("utf-8"))
 tree = ElementTree.parse(file_name)
 root = tree.getroot()
-print(root)
+print(root.tag)
