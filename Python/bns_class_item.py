6fb8f1338cd244b626e987a83a4e8d70170d5329
Bahyun Lee <bahyun.lee@gmail.com>
2016-11-02 01:09:00 +0900

    change code and dict calculator

--- Python/bns_class_item.py
+++ Python/bns_class_item.py
@@ -3,13 +3,14 @@ BNS item tree.
 
 . . .
 """
+from collections import OrderedDict
 
 
 class Item():
     """Item base class."""
 
     name = ""
-    materials = None
+    materials = {}
 
     def __init__(self, item_name, *args):
         """Item class initialize."""
@@ -26,26 +27,23 @@ class Item():
         return self.materials
 
 
-SEONGUN = ["불완전한 성운무기", "성운무기 1단계", "성운무기 2단계", "성운무기 3단계",
-           "성운무기 4단계", "성운무기 5단계", "성운무기 6단계", "성운무기 7단계",
-           "성운무기 8단계", "성운무기 9단계", "성운무기 10단계", "성운무기 11단계",
-           "성운무기 12단계"]
 BASE_SEONGUN = {"영석": 125, "월석": 30, "상승무혼": 3, "백청 진화석": 1, "성운조각": 10,
                 "금": 60}
-MAP_SEONGUN = {
-    1: {"질풍무기": 1},
-    2: {"공허 조각": 3},
-    3: {"공허 조각": 5},
-    4: {"공허 조각": 7},
-    5: {"공허 조각": 9},
-    6: {"공허 조각": 12},
-    7: {"격류무기": 1},
-    8: {"잠룡무기": 1},
-    9: {"수라무기": 1},
-    10: {"초롱무기": 1},
-    11: {"흑룡무기": 1},
-    12: {"설옥무기": 1}
-}
+MAP_SEONGUN = OrderedDict([
+    ("불완전한 성운무기", {}),
+    ("성운무기 1단계", {"질풍무기": 1, "불완전한 성운무기": 1}),
+    ("성운무기 2단계", {"공허 조각": 3, "성운무기 1단계": 1}),
+    ("성운무기 3단계", {"공허 조각": 5, "성운무기 2단계": 1}),
+    ("성운무기 4단계", {"공허 조각": 7, "성운무기 3단계": 1}),
+    ("성운무기 5단계", {"공허 조각": 9, "성운무기 4단계": 1}),
+    ("성운무기 6단계", {"공허 조각": 12, "성운무기 5단계": 1}),
+    ("성운무기 7단계", {"격류무기": 1, "성운무기 6단계": 1}),
+    ("성운무기 8단계", {"잠룡무기": 1, "성운무기 7단계": 1}),
+    ("성운무기 9단계", {"수라무기": 1, "성운무기 8단계": 1}),
+    ("성운무기 10단계", {"초롱무기": 1, "성운무기 9단계": 1}),
+    ("성운무기 11단계", {"흑룡무기": 1, "성운무기 10단계": 1}),
+    ("성운무기 12단계", {"설옥무기": 1, "성운무기 11단계": 1})])
+BASE_CHOKMA = {}
 # CHOKMA = ["촉마무기 1단계", "촉마무기 2단계", "촉마무기 3단계", "촉마무기 4단계",
 #                "촉마무기 5단계", "촉마무기 6단계", "촉마무기 7단계", "촉마무기 8단계",
 #                "촉마무기 9단계"]
@@ -54,26 +52,41 @@ MAP_SEONGUN = {
 
 
 def init():
-    """Item object init function."""
-    list_obj = []
-    for index, item in enumerate(SEONGUN):
-        if item == "불완전한 성운무기":
-            list_obj.append(Item(item))
+    """
+    Item object init function.
+
+    seongun[0] = seoung weapon tuples.
+    seongun[0][0] = seongun weapon name.
+    seongun[0][1] = seongun weapon materials.
+    """
+    list_obj_seongun = []
+    for seongun in zip(MAP_SEONGUN.items()):
+        if seongun[0][0] == "불완전한 성운무기":
+            list_obj_seongun.append(Item(seongun[0][0]))
             continue
 
         add_meterial = BASE_SEONGUN.copy()
-        add_meterial.update(MAP_SEONGUN.get(index))
-        list_obj.append(Item(item, add_meterial))
+        add_meterial.update(MAP_SEONGUN.get(seongun[0][0]))
+        list_obj_seongun.append(Item(seongun[0][0], add_meterial))
 
-    return list_obj
+    return list_obj_seongun
 
 
 def main():
     """Main function."""
     res = init()
-    for item in res:
-        print(item.get_item_name(), item.get_item_material())
 
+    test1 = res[1].get_item_material().copy()
+    test2 = res[2].get_item_material().copy()
+    for k in test1:
+        if k in test2:
+            prev = test2.get(k)
+            test1[k] += prev
+        else:
+            tmp_dict = {k: test1.get(k)}
+            test1.update(tmp_dict)
+
+    print(test1)
 
 if __name__ == '__main__':
     main()
