89e895dcd670fcbbb367e789291820cc5e63cd79
Bahyun Lee <bahyun.lee@gmail.com>
2016-11-20 14:29:47 +0900

    print_info function.

--- Rust/print_socket_number.rs
+++ Rust/print_socket_number.rs
@@ -1,11 +1,17 @@
-use std::net::TcpListener;
+use std::net::{TcpListener, TcpStream};
+
+fn print_info(stream: &TcpStream) {
+    let dest_addr = stream.local_addr();
+    let src_addr = stream.peer_addr();
+    println!("{:?}, {:?}", dest_addr, src_addr)
+}
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:80").unwrap();
     for stream in listener.incoming() {
         match stream {
             Err(log) => println!("{:?}", log),
-            Ok(stream) => println!("{:?}", stream)
+            Ok(stream) => print_info(&stream)
         };
     }
 
