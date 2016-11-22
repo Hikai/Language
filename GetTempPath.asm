d7ee09abe67f0867aed7b11c141fbdef1a569b5f
Bahyun Lee <bahyun.lee@gmail.com>
2016-11-22 15:30:41 +0900

    GetTempPath function psudo code.

---
+++ GetTempPath.asm
@@ -0,0 +1,21 @@
+push ebp
+mov ebp, esp
+sub esp, 400
+; enter 0, N
+mov eax, esp-400
+push eax
+mov eax, esp-200
+push eax
+call GetTempPath
+mov eax, esp-400
+push eax
+push str_format
+call printf
+push 0
+call ExiProcess
+leave
+ret
+; format label
+str_format:
+    db '%s\n', 0Dh, 0Ah, 6
+str_format_end:
