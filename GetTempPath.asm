d2d3ddda27a4bcde9f9691017ea71a30569849a2
Bahyun Lee <bahyun.lee@gmail.com>
2016-11-23 14:40:32 +0900

    esp address

--- GetTempPath.asm
+++ GetTempPath.asm
@@ -2,17 +2,19 @@ push ebp
 mov ebp, esp
 sub esp, 400
 ; enter 0, N
-mov eax, esp-400
+mov eax, [esp-400]
 push eax
-mov eax, esp-200
+mov eax, [esp-200]
 push eax
 call GetTempPath
-mov eax, esp-400
+mov eax, [esp-400]
 push eax
 push str_format
 call printf
 push 0
 call ExiProcess
+pop ebx
+pop eax
 leave
 ret
 ; format label
