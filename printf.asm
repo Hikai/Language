f90d40e90491c0cc9ba7ea6bf8576b87014e3430
Bahyun Lee <bahyun.lee@gmail.com>
2016-11-24 16:20:53 +0900

    with C library asm code.

--- printf.asm
+++ printf.asm
@@ -1,7 +1,21 @@
-org 100h
-mov dx, msg
-mov ah, 9
-int 21h
-mov ah, 4Ch
-int 21h
-msg db 'Hello, world', 0Dh, 0Ah, '$'
+; without C library.
+; org 100h
+; mov dx, msg
+; mov ah, 9
+; int 21h
+; mov ah, 4Ch
+; int 21h
+; msg db 'Hello, world', 0Dh, 0Ah, '$'
+
+; with C library.
+global  _main
+extern  _printf
+
+section .text
+_main:
+    push    message
+    call    _printf
+    add     esp, 4
+    ret
+message:
+    db  'Hello, World', 10, 0
