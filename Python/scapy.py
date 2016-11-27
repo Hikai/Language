cb2c6a3a4313822d0d9ed46b0c14e7c4a214a23d
Bahyun Lee <bahyun.lee@gmail.com>
2016-11-28 02:31:05 +0900

    scapy sniff

--- Python/scapy.py
+++ Python/scapy.py
@@ -6,6 +6,19 @@ Scapy library first.
 from scapy.all import *
 
 
-ip = IP(ttl=10, dst='8.8.8.8')
-del(ip.ttl)
-print(ip)
+def prn_work(x):
+    """Working for print information function."""
+    x.summary()
+    x.show()
+
+
+def main():
+    """Main function."""
+    snf = sniff(prn=lambda x: prn_work(x))
+
+
+if __name__ == "__main__":
+    main()
+# ip = IP(ttl=10, dst='8.8.8.8')
+# del(ip.ttl)
+# print(ip)
