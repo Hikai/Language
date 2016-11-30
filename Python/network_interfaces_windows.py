555538b182efaf2016065fa0e5e41cda18e30892
Bahyun Lee <bahyun.lee@gmail.com>
2016-11-30 23:14:43 +0900

    python version check.

--- Python/network_interfaces_windows.py
+++ Python/network_interfaces_windows.py
@@ -3,20 +3,28 @@ Windows network interfaces.
 
 . . .
 """
-import winreg
+import sys
+
+
+if sys.version_info > (2, 7):
+    from winreg import *
+    # python 3.x import
+else:
+    from winreg import *
+    # python 2.x import
 
 
 SUBKEY = "SYSTEM\\CurrentControlSet\\Control\\Network\\"
 SUBKEY += "{4D36E972-E325-11CE-BFC1-08002BE10318}"
-ROOT = winreg.ConnectRegistry(None, winreg.HKEY_LOCAL_MACHINE)
-KEY = winreg.OpenKey(ROOT, SUBKEY)
+ROOT = ConnectRegistry(None, HKEY_LOCAL_MACHINE)
+KEY = OpenKey(ROOT, SUBKEY)
 
 try:
     count = 0
     while 1:
-        name, value, ty = winreg.EnumValue(KEY, count)
+        name, value, ty = EnumValue(KEY, count)
         print(name, value, ty)
         count += 1
 except WindowsError:
     pass
-winreg.CloseKey(KEY)
+CloseKey(KEY)
