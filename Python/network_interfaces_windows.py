cac485d25d722fc5831085621daf0c02d641970a
Bahyun Lee <bahyun.lee@gmail.com>
2016-12-09 23:17:53 +0900

    get sub interfaces registry function.

--- Python/network_interfaces_windows.py
+++ Python/network_interfaces_windows.py
@@ -6,30 +6,34 @@ Windows network interfaces.
 import _winreg as winreg
 
 
-def running(subkey):
+def get_interfaces_list(subkey):
     """Print registry function."""
     registry = winreg.ConnectRegistry(None, winreg.HKEY_LOCAL_MACHINE)
     key = winreg.OpenKey(registry, subkey, 0, winreg.KEY_ALL_ACCESS)
+    list_if = []
 
     try:
-        count = 0
+        index = 0
         while True:
-            name, value, type = winreg.EnumValue(key, count)
-            print("{}, {}".format(name, value))
+            low_key = winreg.EnumKey(key, index)
+            list_if.append(low_key)
 
-            count += 1
+            index += 1
     except WindowsError:
         pass
 
     winreg.CloseKey(key)
+
+    return list_if
 
 
 def main():
     """Main function."""
     subkey = r"SYSTEM\CurrentControlSet\Control\Network"
     subkey += r"\{4D36E972-E325-11CE-BFC1-08002BE10318}"
-    print(subkey)
-    running(subkey)
+    list_inter = get_interfaces_list(subkey)
+    for interface in list_inter:
+        print(interface)
 
 if __name__ == "__main__":
     main()
