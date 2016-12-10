f37bbaee0592c8bd5d80ca5242d3eb5c0dc22cf4
Bahyun Lee <bahyun.lee@gmail.com>
2016-12-11 03:06:11 +0900

    print function -> name return function.

--- Python/network_interfaces_windows.py
+++ Python/network_interfaces_windows.py
@@ -19,6 +19,7 @@ def get_interfaces_list(subkey):
             list_if.append(low_key)
 
             index += 1
+
     except WindowsError:
         pass
 
@@ -27,32 +28,47 @@ def get_interfaces_list(subkey):
     return list_if
 
 
-def print_interfaces_name(subkey, list_inter):
-    """Print interfaces name."""
-    for item_if in list_inter:
+def get_interface_name(interface, subkey):
+    """Return interface name."""
+    subkey += '\\' + interface + r"\Connection"
+
+    try:
         registry = winreg.ConnectRegistry(None, winreg.HKEY_LOCAL_MACHINE)
-        subkey += r"\{}".format(item_if)
         key = winreg.OpenKey(registry, subkey, 0, winreg.KEY_ALL_ACCESS)
 
         try:
             index = 0
             while True:
-                name = winreg.EnumValue(key, index)
-                print(unicode(name))
+                name_conn = winreg.EnumValue(key, index)
+                if name_conn[0] != "Name":
+                    pass
+                else:
+                    return name_conn[1]
 
                 index += 1
+
         except WindowsError:
             pass
 
-    winreg.Close(key)
+    except WindowsError:
+        print("Not interface registry.")
+
+        return
+
+    winreg.CloseKey(key)
 
 
 def main():
     """Main function."""
     subkey = r"SYSTEM\CurrentControlSet\Control\Network"
     subkey += r"\{4D36E972-E325-11CE-BFC1-08002BE10318}"
+
     list_inter = get_interfaces_list(subkey)
-    print_interfaces_name(subkey, list_inter)
+    list_name = []
+    for interface in list_inter:
+        list_name.append(get_interface_name(interface, subkey))
+
+    print(list_name)
 
 if __name__ == "__main__":
     main()
