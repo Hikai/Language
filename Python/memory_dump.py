a46ce1a71c3f5f7ea868366d85aefed5c558dee4
Bahyun Lee <bahyun.lee@gmail.com>
2016-12-27 16:47:32 +0900

    write debugger class.

--- Python/memory_dump.py
+++ Python/memory_dump.py
@@ -3,7 +3,60 @@ Python a few second memory dump script.
 
 . . . (windows)
 """
+from _multiprocessing import win32
+from ctypes import byref, c_char_p, c_ulong, windll
 import psutil
+
+
+KERNEL = windll.kernel32
+
+
+class Debugger():
+    """Debugging class."""
+
+    def __init__(self, pid):
+        """Debugger class: Initialize method."""
+        self.hnd = None
+        self.debug_active = False
+        self.pid = pid
+
+    def attach(self):
+        """Debugger class: Process attach method."""
+        self.hnd = KERNEL.OpenProcess(win32.PROCESS_ALL_ACCESS, False,
+                                      self.pid)
+
+        if KERNEL.DebugActiveProcess(self.pid):
+            self.debug_active = True
+            print("Attaching start.")
+        else:
+            print("Fail to attach.")
+            self.get_last_error()
+
+    def dettach(self):
+        """Debugger class: Process dettach method."""
+        if KERNEL.DebugActiveProcessStop(self.pid):
+            print("Stop debugging.")
+        else:
+            print("Fail to dettach.")
+            self.get_last_error()
+
+    def read_memory(self):
+        """Debugger class: Process memory read method."""
+        adr = 0x100000
+        buf = c_char_p("data")
+        size_buf = len(buf.value)
+        bytes_read = c_ulong(0)
+
+        if KERNEL.ReadProcessMemory(self.hnd, adr, buf, size_buf,
+                                    byref(bytes_read)):
+            print("Memory: {}".format(buf))
+        else:
+            print("Fail to read memory.")
+            self.get_last_error()
+
+    def get_last_error(self):
+        """Debugger class: Last error method."""
+        print("Error: {}".format(KERNEL.GetLastError()))
 
 
 def check_process():
@@ -25,7 +78,10 @@ def main():
         else:
             break
 
-    print(pid)
+    debug = Debugger(pid)
+    debug.attach()
+    debug.read_memory()
+    debug.dettach()
 
 
 if __name__ == "__main__":
