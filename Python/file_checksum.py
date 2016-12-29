108e921c3ae8b1efc1f7a683ffa2c528d05a2eb5
Bahyun Lee <bahyun.lee@gmail.com>
2016-12-30 01:31:26 +0900

    file checksum example.

---
+++ Python/file_checksum.py
@@ -0,0 +1,34 @@
+"""
+Get file checksum.
+
+. . .
+"""
+import hashlib
+import os
+import sys
+
+
+def get_file_size(path_file):
+    """Return file size function."""
+    return os.stat(path_file).st_size
+
+
+def main(path_file):
+    """Main function."""
+    size_file = get_file_size(path_file)
+    hash_md5 = hashlib.md5()
+    with open(path_file, "rb") as file:
+        if size_file > 4096:
+            for chunk in iter(lambda: file.read(4096), b''):
+                hash_md5.update(chunk)
+        else:
+            hash_md5.update(file.read())
+
+    print(hash_md5.hexdigest())
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        exit(1)
+
+    main(sys.argv[1])
