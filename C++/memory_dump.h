7150000e467751564cab4f97f2c9d0cbe5e711c3
Bahyun Lee <bahyun.lee@gmail.com>
2017-01-05 17:53:59 +0900

    edit

--- C++/memory_dump.h
+++ C++/memory_dump.h
@@ -8,51 +8,55 @@
 
 #include<iostream>
 #include<windows.h>
+#include<string.h>
 #include<psapi.h>
 
 #define ARR_MAX 1024
 
 BOOL check_process();
-std::string get_name_by_pid(DWORD pid);
+void set_name_by_pid(_In_ DWORD pid);
+
+TCHAR name_proc[ARR_MAX];
 
 class Debugger {
 private:
 	HANDLE hnd_proc = NULL;
 	BOOL debug_active = false;
-	int pid = 0;
+	DWORD pid = 0;
 
 public:
-	Debugger(int pid);
+	Debugger(_In_ DWORD);
 	void attach();
 	void dettach();
 	void read_memory();
+	void set_privilege(_In_ bool);
 };
 
 BOOL check_process()
 {
 	DWORD arr_process[ARR_MAX], bytes_proc, max_proc;
-	std::string name_proc = nullptr;
 	if (!EnumProcesses(arr_process, sizeof(arr_process), &bytes_proc)) {
 		std::cout << "Process enumerate failed." << std::endl;
 		return false;
 	}
-	
+
 	max_proc = bytes_proc / sizeof(DWORD);
 
 	for (unsigned int i = 0; i < max_proc; i++) {
 		if (arr_process[i] != 0) {
-			name_proc = get_name_by_pid(arr_process[i]);
-			std::cout << name_proc.c_str() << std::endl;
+			set_name_by_pid(arr_process[i]);
+			if (!wcscmp(name_proc, TEXT("cmd.exe"))) {
+				printf("%ws\n", name_proc);
+				break;
+			}
 		}
 	}
 
 	return true;
 }
 
-std::string get_name_by_pid(DWORD pid)
+void set_name_by_pid(_In_ DWORD pid)
 {
-	TCHAR name_proc[ARR_MAX] = TEXT("unknown");
-	
 	HANDLE proc = OpenProcess(PROCESS_VM_READ | PROCESS_QUERY_INFORMATION, FALSE, pid);
 	if (proc != NULL) {
 		HMODULE module;
@@ -63,21 +67,16 @@ std::string get_name_by_pid(DWORD pid)
 		}
 	}
 	CloseHandle(proc);
-
-	std::wstring arr_wstr(name_proc);
-	std::string arr_str(arr_wstr.begin(), arr_wstr.end());
-
-	return arr_str;
 }
 
-Debugger::Debugger(int pid)
+Debugger::Debugger(_In_ DWORD pid)
 {
-
+	this->pid = pid;
 }
 
 void Debugger::attach()
 {
-
+	this->hnd_proc = OpenProcess(PROCESS_ALL_ACCESS, false, this->pid);
 }
 
 void Debugger::dettach()
@@ -86,6 +85,11 @@ void Debugger::dettach()
 }
 
 void Debugger::read_memory()
+{
+
+}
+
+void Debugger::set_privilege(bool valid)
 {
 
 }
