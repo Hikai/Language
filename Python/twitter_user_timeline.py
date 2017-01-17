8d0d24c28173a20c978c6dbb48e0551d44540d1b
Bahyun Lee <bahyun.lee@gmail.com>
2017-01-17 20:44:09 +0900

    timeline content

--- Python/twitter_user_timeline.py
+++ Python/twitter_user_timeline.py
@@ -8,16 +8,17 @@ Jobs:
 from twitter import OAuth, Twitter
 import json
 
-ACCESS_TOKEN = 'ACCESS_TOKEN'
-ACCESS_SECRET = 'ACCESS_SECRET'
-CONSUMER_KEY = 'CONSUMER_KEY'
-CONSUMER_SECRET = 'CONSUMER_SECRET'
+ACCESS_TOKEN = '1660682539-nwLJu95X57ePGSJNn7wBSyzFzYCb6vVfrsbZyvo'
+ACCESS_SECRET = 'mWzTNLDPjvJrQP2JCTzPdfHATxwU5ZDS8kDDALVJvRDFk'
+CONSUMER_KEY = 'cHS3ZmJPzhTn5T6HNA7d2nYLh'
+CONSUMER_SECRET = 'LZK0BB7TI8SclFmxlPMBU0CUbvL8pBDsvmRCd87ioVRjNVoGUs'
 oauth = OAuth(ACCESS_TOKEN, ACCESS_SECRET, CONSUMER_KEY, CONSUMER_SECRET)
 twitter = Twitter(auth=oauth)
-tl = twitter.statuses.user_timeline(screen_name="user_id")
+tl = twitter.statuses.user_timeline(screen_name="ctf_sister_bot", count=10000)
 count = 1000
 for tweet in tl:
     count -= 1
     print(json.dumps(tweet, sort_keys=True, indent=4, separators=(',', ': ')))
+    print(json.dumps(tweet["text"]))
     if count <= 0:
         break
