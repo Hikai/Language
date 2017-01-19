aa061d901ed46a3ae074573d636b266b106920ac
Bahyun Lee <bahyun.lee@gmail.com>
2017-01-20 01:13:58 +0900

    ?

--- C++/memory_dump.cpp
+++ C++/memory_dump.cpp
@@ -1,21 +1,15 @@
 #include"memory_dump.h"
 
-int main(int argc, char ** argv)
+int main(void)
 {
-    if (argc != 2) {
-        std::cout << "Usage: memory_dump.exe [pid]" << std::endl;
-        
-        return 0;
+    DWORD pid = 0;
+
+    std::cout << "Search process." << std::endl;
+    while (pid == 0) {
+        pid = check_process(L"cmd.exe");
     }
-
-    DWORD pid = atoi(argv[1]);
-
-    //std::cout << "Search process." << std::endl;
-    //while (pid == 0) {
-    //  check_process(&pid);
-    //}
-    
-    //std::cout << pid << " found process." << std::endl;
+     
+    std::cout << pid << " found process id." << std::endl;
 
     Debugger debugger(pid);
     debugger.attach();
