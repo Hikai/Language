7be36e63d59da3ebfa8c1ab1d66ef9d8404998af
Bahyun Lee <bahyun.lee@gmail.com>
2017-01-24 17:39:17 +0900

    selenium module

--- Python/shinseok_shop.py
+++ Python/shinseok_shop.py
@@ -1,11 +1,17 @@
+# -*- coding: cp949 -*-
+# -*- coding: utf-8 -*-
 """
 Shinseok-shop parse.
 
 . . .
 """
 from bs4 import BeautifulSoup
-from urllib2 import urlopen
-from time import sleep
+from selenium import webdriver
+import re
+
+
+# RE_TITLE = re.compile('<a class="grade_[1-9]" href="(?:.*)">(.*)</a>')
+RE_SS = re.compile("<em>(.*)</em>")
 
 
 class Parse():
@@ -13,27 +19,32 @@ class Parse():
 
     def __init__(self, url):
         """Initialize method."""
-        self.source = urlopen(url)
-        sleep(3)
-        self.soup = BeautifulSoup(self.source.read(), "html.parser")
+        driver = webdriver.Firefox()
+        driver.get(url)
+        self.source = driver.page_source
+        driver.quit()
+        self.soup = BeautifulSoup(self.source, "html.parser")
 
     def parse_title(self):
         """Parse shinseok-shop today item title method."""
-        names = self.soup.findall('a', {"class": "grade_5"})
+        names = self.soup.findAll("h2", {"class": "title"})
         for name in names:
+            # print(name.contents)
             print(name)
 
     def parse_shinseok(self):
         """Parse shinseok-shop today item shinseok method."""
-        shinseoks = self.soup.findall("span", {"class": "shinseok"})
+        shinseoks = self.soup.findAll("span", {"class": "shinseok"})
         for shinseok in shinseoks:
-            print(shinseok)
+            num_shinseok = filter(RE_SS.match, shinseok.contents)
+            print(str(num_shinseok))
 
 
 def main():
     """Main function."""
     parser = Parse("http://bns.plaync.com")
-    print(parser.soup)
+    parser.parse_title()
+    parser.parse_shinseok()
 
 
 if __name__ == "__main__":
