6502d4a9eec17a0372fadddf522d6ba5de0632f7
Bahyun Lee <bahyun.lee@gmail.com>
2017-01-29 19:43:24 +0900

    selector issue

--- Python/shinseok_shop_req.py
+++ Python/shinseok_shop_req.py
@@ -15,24 +15,28 @@ class Parse():
         """Initalize method."""
         source = requests.get(url)
         self.soup = BeautifulSoup(source.text, "html.parser")
-        self.soup = self.soup.select("body > div > div > ul > li")
+        self.soup_select = self.soup.select("body > div > div > ul > li")
+        # select issue
 
     def parse_data(self):
         """Parse data method."""
-        dict_item = {}
-        for li in self.soup:
-            name = li.select('a')[0].string
-            shinseok = li.select("em")[0].string
-            dict_item.update({name: shinseok})
+        # dict_item = {}
+        # for li in self.soup:
+        #     name = li.select('a')[0].string
+        #     shinseok = li.select("em")[0].string
+        #     dict_item.update({name: shinseok})
 
-        return dict_item
+        # return dict_item
+        print(self.soup)
+        print("-------")
+        print(self.soup_select)  # print "[]"
 
 
 def main():
     """Main function."""
     url = "http://bns.plaync.com/login/displayItemList?_=1485444240419"
     parser = Parse(url)
-    print(parser.parse_data())
+    parser.parse_data()
 
 if __name__ == "__main__":
     main()
