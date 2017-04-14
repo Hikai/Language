9ff0085048762d37f592b18620484016c540d79e
Hikai <hikai.bep@gmail.com>
2017-04-14 10:10:26 +0900

    main

--- Python/reverse_script.py
+++ Python/reverse_script.py
@@ -1,8 +1,15 @@
 # -*- coding: utf-8 -*-
 
 if __name__ == "__main__":
+    list_str = []
     strings = "abcdefgh"
     tmp = 0
     for i in range(3, len(strings) + 3, 3):
-        print(strings[tmp:i], end=" ")
+        list_str.append(strings[tmp:i])
         tmp = i
+
+    for strings in list_str:
+        cry = 10
+        for i, char in enumerate(strings):
+            char_hex = int(format(ord(char), "x"))
+            print(char_hex + (cry * i))
