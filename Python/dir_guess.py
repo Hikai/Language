829586d20936d7bde1c2a16558875cbfea49b4af
Hikai <hikai.bep@gmail.com>
2017-04-20 20:58:35 +0900

    update

--- Python/dir_guess.py
+++ Python/dir_guess.py
@@ -14,8 +14,16 @@ class Crawl():
         """Initialize method."""
         self.url = url
         req = requests.get(self.url)
-        print(req.status_code)
+        self.check_status_code(req.status_code)
+
+    def check_status_code(self, status_code):
+        """Method check http status code."""
+        num_front = int(status_code / 100)
+        if num_front == 2:
+            return True
+        else:
+            return False
 
 
 if __name__ == "__main__":
-    Crawl()
+    Crawl("http://www.naver.com")
