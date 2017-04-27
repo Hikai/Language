de6a10836895d3105e7c71b6d43a10bd59dae2d9
Hikai <bahyun.lee@gmail.com>
2017-04-27 16:35:03 +0900

    library name mistyping

--- fmk_install.sh
+++ fmk_install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo apt-get update
-sudo apt-get -y install build-essential zlib1g-dev lilzma-dev python-magic
+sudo apt-get -y install build-essential zlib1g-dev liblzma-dev python-magic
 wget https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/firmware-mod-kit/fmk_099.tar.gz
 tar fzx fmk_099.tar.gz
