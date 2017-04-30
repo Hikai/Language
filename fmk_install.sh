493f240597776406565850d51dde0d99b48f9ecb
Hikai <hikai.bep@gmail.com>
2017-04-30 19:40:13 +0900

    undo

--- fmk_install.sh
+++ fmk_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sudo apt-get update
+sudo apt-get -y update
+sudo apt-get -y upgrade
 sudo apt-get -y install build-essential zlib1g-dev liblzma-dev python-magic
-# wget https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/firmware-mod-kit/fmk_099.tar.gz
-# tar fzx fmk_099.tar.gz
-git clone https://github.com/rampageX/firmware-mod-kit.git
+wget https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/firmware-mod-kit/fmk_099.tar.gz
+tar fzx fmk_099.tar.gz
