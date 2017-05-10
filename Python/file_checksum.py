ff5f526b7152bdafca0ad1aa9284937b4a7eb3e9
Hikai <hikai.bep@gmail.com>
2017-05-10 20:27:59 +0900

    class working

--- Python/file_checksum.py
+++ Python/file_checksum.py
@@ -8,27 +8,35 @@ import os
 import sys
 
 
-def get_file_size(path_file):
-    """Return file size function."""
-    return os.stat(path_file).st_size
+class Check_calc():
+    """Calculator checksum class."""
+
+    def __init__(self):
+        """Initialize method."""
+        pass
 
 
-def main(path_file):
-    """Main function."""
-    size_file = get_file_size(path_file)
-    hash_md5 = hashlib.md5()
-    with open(path_file, "rb") as file:
-        if size_file > 4096:
-            for chunk in iter(lambda: file.read(4096), b''):
-                hash_md5.update(chunk)
-        else:
-            hash_md5.update(file.read())
+# def get_file_size(path_file):
+#     """Return file size function."""
+#     return os.stat(path_file).st_size
 
-    print(hash_md5.hexdigest())
+
+# def main(path_file):
+#     """Main function."""
+#     size_file = get_file_size(path_file)
+#     hash_md5 = hashlib.md5()
+#     with open(path_file, "rb") as file:
+#         if size_file > 4096:
+#             for chunk in iter(lambda: file.read(4096), b''):
+#                 hash_md5.update(chunk)
+#         else:
+#             hash_md5.update(file.read())
+
+#     print(hash_md5.hexdigest())
 
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         exit(1)
 
-    main(sys.argv[1])
+    # main(sys.argv[1])
