a672f99d6e3f2ee01b34b2d1474c3a0652cc641f
Hikai <hikai.bep@gmail.com>
2017-05-11 14:29:10 +0900

    class !

--- Python/file_checksum.py
+++ Python/file_checksum.py
@@ -8,35 +8,39 @@ import os
 import sys
 
 
-class Check_calc():
+class Checksum():
     """Calculator checksum class."""
 
-    def __init__(self):
+    def __init__(self, path_file):
         """Initialize method."""
-        pass
+        self.path_file = path_file
+
+    def get_size(self):
+        """Method return file size."""
+        return os.stat(self.path_file).st_size
+
+    def get_file_hash(self):
+        """Method return file checksum."""
+        size_file = self.get_size()
+        hash_md5 = hashlib.md5()
+
+        with open(self.path_file, "rb") as target:
+            if size_file > 4096:
+                for chunk in iter(lambda: target.read(4096), b''):
+                    hash_md5.update(chunk)
+            else:
+                hash_md5.update(target.read())
+
+        return hash_md5.hexdigest()
 
 
-# def get_file_size(path_file):
-#     """Return file size function."""
-#     return os.stat(path_file).st_size
-
-
-# def main(path_file):
-#     """Main function."""
-#     size_file = get_file_size(path_file)
-#     hash_md5 = hashlib.md5()
-#     with open(path_file, "rb") as file:
-#         if size_file > 4096:
-#             for chunk in iter(lambda: file.read(4096), b''):
-#                 hash_md5.update(chunk)
-#         else:
-#             hash_md5.update(file.read())
-
-#     print(hash_md5.hexdigest())
-
+def main(path_file):
+    """Main function."""
+    checksum_file = Checksum(path_file)
+    print(checksum_file.get_file_hash())
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         exit(1)
 
-    # main(sys.argv[1])
+    main(sys.argv[1])
