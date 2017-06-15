53974ba58b9e0026a6572b1a526dcb5cd98c6e47
Hikai <bahyun.lee@gmail.com>
2017-06-15 09:27:16 +0900

    clear ?

--- Python/exc_handler.py
+++ Python/exc_handler.py
@@ -4,12 +4,34 @@ Exception global hook library.
 
 @hikai
 """
+import subprocess
 import sys
 
-def except_handler(except_type, value, traceback):
+
+def print_fail(msg):
+    """Error print."""
+    print(msg)
+    exit(1)
+
+def except_handler(except_type, detail, traceback):
     """Exception handler function."""
-	print("Exception:", except_type)
-	print("Value:", value)
-	print("Traceback:", traceback)
+    if "ModuleNotFoundError" not in str(except_type):
+        print_fail("Exception type: {}\nException detail: {}\nTraceback: {}".format(except_type, detail, traceback))
+
+    details = str(detail).split(' ')
+    name_module = details[-1].replace('\'', '')
+
+    args = ["pip", "install", name_module]
+    try:
+        proc = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        result, err = proc.communicate()
+    except:
+        print_fail("Not install pip.")
+
+    if "No matching" in str(err):
+        print_fail("Not found module.")
+
+    print("Install complete! Retry script!")
+    exit(0)
 
 sys.excepthook = except_handler
