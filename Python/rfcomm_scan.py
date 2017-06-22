a09839d0b9f69e3d85be70b784005b57460d5f1b
Hikai <bahyun.lee@gmail.com>
2017-06-22 17:37:24 +0900

    Create rfcomm_scan.py

---
+++ Python/rfcomm_scan.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from bluetooth import *
+import sys
+"""
+rfcomm scanner.
+
+thx google, thx github.
+"""
+
+
+def rfcommCon(addr, port):
+    sock = BluetoothSocket(RFCOMM)
+    try:
+        sock.connect((addr, port))
+        print("[+] RFCOMM Port " + str(port) + " open")
+        sock.close()
+    except Exception, e:
+        print("[-] RFCOMM Port " + str(port) + " closed")
+
+def main(addr_mac):
+    for port in range(1, 30):
+        rfcommCon('00:16:38:DE:AD:11', port)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2 or len(sys.argv[1].split(':')) != 6:
+        exit(-1)
+
+    main(sys.argv[1])
