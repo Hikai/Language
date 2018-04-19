042bffcb84f7b78f55bc33cad4b7f7a286f9f8bb
Hikai <hikai.bep@gmail.com>
2018-04-19 10:41:41 +0900

    Update threading and save path.

--- Python/discord_image_crawl_bot.py
+++ Python/discord_image_crawl_bot.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
+from os.path import expanduser, join, isdir
 import asyncio
 import discord
 import requests
 import shutil
+import threading
 
 client = discord.Client()
-path_save = ""
+path_save = join(expanduser('~'), "Downloads", "Images")
 
 
 @client.event
@@ -35,6 +37,11 @@ async def on_message(message):
 
             dict_var = log.attachments[0]
             counter = 0
+
+            if not isdir(path_save):
+                from os import makedirs
+                makedirs(path_save)
+
             if dict_var['filename'].startswith('unknown'):
                 ext = dict_var['filename'].split('.')[1]
                 dict_var['filename'] = "unknown{}.{}".format(counter, ext)
@@ -42,12 +49,18 @@ async def on_message(message):
 
             r = requests.get(dict_var['url'], stream=True)
             if r.status_code == 200:
-                with open(dict_var['filename'], 'wb') as f:
-                    r.raw.decode_content = True
-                    shutil.copyfileobj(r.raw, f)
+                t = threading.Thread(target=save_file, args=(dict_var['filename'], r))
+                t.start()
+
             else:
                 print("Failed {}\n{}".format(r.status_code, dict_var['url']))
 
         print("End of the image crawl.")
+
+
+def save_file(name_file, req):
+    with open(join(path_save, name_file), "wb") as f:
+        req.raw.decode_content = True
+        shutil.copyfileobj(req.raw, f)
 
 client.run('token')
